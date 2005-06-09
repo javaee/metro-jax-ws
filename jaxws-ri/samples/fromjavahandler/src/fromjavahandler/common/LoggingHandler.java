341c903f7af3f406a8758feba2c0dd12744005a6
Arun Gupta <arun.p.gupta@oracle.com>
2005-06-09 19:48:19 +0000

    Here is the actual handler
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

---
+++ jaxws-ri/samples/fromjavahandler/src/fromjavahandler/common/LoggingHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc.
+ * All rights reserved. 
+ */
+package fromjavahandler.common;
+
+
+import java.io.PrintStream;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+/*
+ * This simple SOAPHandler will output the contents of incoming
+ * and outgoing messages.
+ */
+public class LoggingHandler implements SOAPHandler<SOAPMessageContext> {
+
+    // change this to redirect output if desired
+    private static PrintStream out = System.out;
+
+    // used to hold initilization data
+    private Map config;
+    
+    public void init(Map config) {
+        this.config = config;
+    }
+    
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    public boolean handleMessage(SOAPMessageContext smc) {
+        logToSystemOut(smc);
+        return true;
+    }
+
+    public boolean handleFault(SOAPMessageContext smc) {
+        logToSystemOut(smc);
+        return true;
+    }
+
+    // nothing to clean up
+    public void close(MessageContext messageContext) {
+    }
+
+    // nothing to clean up
+    public void destroy() {
+    }
+
+    /*
+     * Check the MESSAGE_OUTBOUND_PROPERTY in the context
+     * to see if this is an outgoing or incoming message.
+     * Write a brief message to the print stream and
+     * output the message. The writeTo() method can throw
+     * SOAPException or IOException
+     */
+    private void logToSystemOut(SOAPMessageContext smc) {
+        Boolean outboundProperty = (Boolean)
+            smc.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        
+        if (outboundProperty.booleanValue()) {
+            out.println("\nOutbound message:");
+        } else {
+            out.println("\nInbound message:");
+        }
+        
+        SOAPMessage message = smc.getMessage();
+        try {
+            message.writeTo(out);
+            out.println("");   // just to add a newline
+        } catch (Exception e) {
+            out.println("Exception in handler: " + e);
+        }
+    }
+}
