b014215d2cd0e297fa64e6e85fd33d3ec144a6ec
Vivek Pandey <vivek.pandey@sun.com>
2005-06-15 01:20:45 +0000

    SOAP 1.2 sample starting from JAVA. Its same as fromjava sample except SOAP binding is 1.2.

---
+++ jaxws-ri/samples/fromjava-soap12/src/fromjava_soap12/client/AddNumbersClient.java
@@ -0,0 +1,35 @@
+/*
+* Copyright (c) 2005 Sun Microsystems, Inc.
+* All rights reserved. 
+*/
+package fromjava_soap12.client;
+
+import javax.xml.ws.ServiceFactory;
+
+import java.rmi.RemoteException;
+
+public class AddNumbersClient {
+    public static void main(String[] args) {
+        try {
+            ServiceFactory serviceFactory = ServiceFactory.newInstance();
+            AddNumbersImplService service = (AddNumbersImplService)serviceFactory.createService((java.net.URL)null, AddNumbersImplService.class);
+            AddNumbersImpl port = service.getAddNumbersImpl();
+			
+            int number1 = 10;
+            int number2 = 20;
+
+            System.out.printf("Invoking addNumbers(%d, %d)\n", number1, number2);
+            int result = port.addNumbers(number1, number2);
+            System.out.printf("The result of adding %d and %d is %d.\n\n", number1, number2, result);
+
+            number1 = -10;
+            System.out.printf("Invoking addNumbers(%d, %d)\n", number1, number2);
+            result = port.addNumbers(number1, number2);
+            System.out.printf("The result of adding %d and %d is %d.\n", number1, number2, result);
+	} catch (RemoteException ex) {
+            ex.printStackTrace();
+        } catch (AddNumbersException_Exception ex) {
+            System.out.printf("Caught AddNumbersException_Exception: %s\n", ex.getFaultInfo().getDetail());
+	}
+    }
+}
---
+++ jaxws-ri/samples/fromjava-soap12/src/fromjava_soap12/server/AddNumbersException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc.
+ * All rights reserved. 
+ */
+package fromjava_soap12.server;
+
+public class AddNumbersException extends Exception {
+    String detail;
+
+    public AddNumbersException(String message, String detail) {
+        super(message);
+        this.detail = detail;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+}
