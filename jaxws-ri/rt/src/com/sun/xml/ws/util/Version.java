7dee08dd200dc0a0bb972da2d55241ddf9616686
Doug Kohlert <doug.kohlert@sun.com>
2005-06-21 19:15:09 +0000

    updated version

--- jaxws-ri/rt/src/com/sun/xml/ws/util/Version.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/Version.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Version.java,v 1.3 2005-06-17 20:16:07 kohlert Exp $
+ * $Id: Version.java,v 1.4 2005-06-21 19:15:09 kohlert Exp $
  */
 
 /*
@@ -32,5 +32,5 @@ public interface Version {
 	 */
         // TODO change this back to R11 for the FCS release.
 //	public static final String BUILD_NUMBER = "R11";
-	public static final String BUILD_NUMBER = "EA6";
+	public static final String BUILD_NUMBER = "EA2_B3";
 }
