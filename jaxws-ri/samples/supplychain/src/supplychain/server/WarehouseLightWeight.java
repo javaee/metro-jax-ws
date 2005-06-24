95b0a402e9dbe8d724715a49c6cd5516b8898b90
Arun Gupta <arun.p.gupta@oracle.com>
2005-06-24 19:22:14 +0000

    - Added light-weight server for supplychain sample - j2se.sei property in etc/build.properties specifies the class name for   the light weight server - server-j2se builds and starts the server
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

---
+++ jaxws-ri/samples/supplychain/src/supplychain/server/WarehouseLightWeight.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2005 Sun Microsystems, Inc.
+ * All rights reserved. 
+ */
+package supplychain.server;
+
+import javax.xml.ws.EndpointFactory;
+
+public class WarehouseLightWeight {
+
+    public static void main(String[] args) throws Exception {
+        EndpointFactory.newInstance().publish(
+            "http://localhost:8080/jaxws-supplychain/submitpo",
+            new WarehouseImpl());
+    }
+}
