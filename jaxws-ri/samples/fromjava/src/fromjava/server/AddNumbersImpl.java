c84d62507ad698e54b79129a18aa1ea3370def47
Arun Gupta <arun.p.gupta@oracle.com>
2005-07-11 18:36:55 +0000

    Some more edits
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- jaxws-ri/samples/fromjava/src/fromjava/server/AddNumbersImpl.java
+++ jaxws-ri/samples/fromjava/src/fromjava/server/AddNumbersImpl.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2005 Sun Microsystems, Inc.
- * All rights reserved. 
+ * All rights reserved.
  */
 package fromjava.server;
 
 import javax.jws.WebService;
 
 @WebService
-public class AddNumbersImpl {
-	
+    public class AddNumbersImpl {
+    
     /**
      * @param number1
      * @param number2
@@ -16,11 +16,11 @@ public class AddNumbersImpl {
      * @throws AddNumbersException
      *             if any of the numbers to be added is negative.
      */
-    public int addNumbers(int number1, int number2) throws AddNumbersException {
-	if (number1 < 0 || number2 < 0) {
-            throw new AddNumbersException("Negative number cant be added!",
-					"Numbers: " + number1 + ", " + number2);
-	}
-	return number1 + number2;
+    public int addNumbers (int number1, int number2) throws AddNumbersException {
+        if (number1 < 0 || number2 < 0) {
+            throw new AddNumbersException ("Negative number cant be added!",
+                "Numbers: " + number1 + ", " + number2);
+        }
+        return number1 + number2;
     }
 }
