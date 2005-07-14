43e102bbd860d9796ac3a4378659a0148696fa38
Ken Walsh <ken.walsh@sun.com>
2005-07-14 20:19:16 +0000

    Issue number: Obtained from: Submitted by: Reviewed by: CVS: Now use SOAPXMLEncoders/Decoders

--- jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/impl/DispatchContactInfoList.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/client/dispatch/impl/DispatchContactInfoList.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DispatchContactInfoList.java,v 1.3 2005-06-09 15:51:32 kwalsh Exp $
+ * $Id: DispatchContactInfoList.java,v 1.4 2005-07-14 20:19:16 kwalsh Exp $
  *
  * Copyright (c) 2004 Sun Microsystems, Inc.
  * All rights reserved.
@@ -10,13 +10,17 @@ package com.sun.xml.ws.client.dispatch.impl;
 
 import com.sun.pept.ept.ContactInfoList;
 import com.sun.pept.ept.ContactInfoListIterator;
-import com.sun.xml.ws.client.ContactInfoListIteratorBase;
+import com.sun.xml.ws.client.*;
 import com.sun.xml.ws.client.dispatch.impl.encoding.DispatchSOAP12XMLDecoder;
 import com.sun.xml.ws.client.dispatch.impl.encoding.DispatchSOAP12XMLEncoder;
 import com.sun.xml.ws.client.dispatch.impl.encoding.DispatchXMLEncoder;
 import com.sun.xml.ws.client.dispatch.impl.encoding.DispatchXMLDecoder;
 import com.sun.xml.ws.client.dispatch.impl.protocol.MessageDispatcherHelper;
 import com.sun.xml.ws.encoding.soap.message.SOAPMessageContext;
+import com.sun.xml.ws.encoding.soap.*;
+import com.sun.xml.ws.encoding.soap.SOAP12XMLEncoder;
+import com.sun.xml.ws.encoding.soap.SOAPXMLDecoder;
+import com.sun.xml.ws.encoding.soap.SOAPXMLEncoder;
 
 import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.soap.SOAPBinding;
@@ -37,12 +41,12 @@ public class DispatchContactInfoList implements ContactInfoList {
         //needed
         arrayList.add(new DispatchContactInfo(null,
             new MessageDispatcherHelper(),
-            new DispatchXMLEncoder(),
-            new DispatchXMLDecoder(),SOAPBinding.SOAP11HTTP_BINDING));
+            new SOAPXMLEncoder(),
+            new SOAPXMLDecoder(),SOAPBinding.SOAP11HTTP_BINDING));
         arrayList.add(new DispatchContactInfo(null,
             new MessageDispatcherHelper(),
-            new DispatchSOAP12XMLEncoder(),
-            new DispatchSOAP12XMLDecoder(), SOAPBinding.SOAP12HTTP_BINDING));
+            new SOAP12XMLEncoder(),
+            new com.sun.xml.ws.encoding.soap.SOAP12XMLDecoder(), SOAPBinding.SOAP12HTTP_BINDING));
         /*arrayList.add(new DispatchContactInfo(null,
                 new MessageDispatcherHelper(new DispatchEncoderDecoderUtil()),
                 new SOAPFastEncoder(new DispatchEncoderDecoderUtil()),
