97f0fbcfdac395faba33abcecc3fb4cdfe4d46b4
Doug Kohlert <doug.kohlert@sun.com>
2005-07-15 00:34:01 +0000

    added some javadocs

--- jaxws-ri/rt/src/com/sun/xml/ws/server/Tie.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/server/Tie.java
@@ -1,5 +1,5 @@
 /**
- * $Id: Tie.java,v 1.3 2005-07-13 21:21:15 jitu Exp $
+ * $Id: Tie.java,v 1.4 2005-07-15 00:34:01 kohlert Exp $
  *
  * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -21,8 +21,28 @@ import com.sun.xml.ws.spi.runtime.WSConnection;
 import com.sun.xml.ws.spi.runtime.SystemHandlerDelegate;
 import com.sun.xml.ws.util.MessageInfoUtil;
 
+/**
+ * Entry point for all server requests.
+ *
+ * @author WS Development Team
+ */
 public class Tie implements com.sun.xml.ws.spi.runtime.Tie {
     
+    /**
+     * Common entry point for server runtime. 
+     * creates a MessageInfo for every Request/Response.
+     * creates a RuntimeContext for every Request/Response and sets that as a metadata in 
+     * MessageInfo. Don't create any other metadata on MessageInfo. If anything is needed, 
+     * that can be created on RuntimeContext
+     * EPTFactoryFactoryBase is used to select a correct EPTFactory
+     * Calls MessageDispatcher.receive(MessageInfo). 
+     * MessageDispatcher orchestrates all the flow: reading from WSConnection, 
+     * decodes message to parameters, invoking implementor, encodes parameters to message, 
+     * and writing to WSConnection
+     * @param connection encapsulates multiple transports
+     * @param endpoint has all the information about target endpoint
+     * @throws Exception throws Exception if any error occurs
+     */
     public void handle(WSConnection connection,
         com.sun.xml.ws.spi.runtime.RuntimeEndpointInfo endpoint)
     throws Exception {
