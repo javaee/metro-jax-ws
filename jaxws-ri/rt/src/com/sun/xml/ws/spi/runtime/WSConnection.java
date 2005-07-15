b19b32c9564fcfa505145f60a04e812f0fca8c2c
Doug Kohlert <doug.kohlert@sun.com>
2005-07-15 22:38:27 +0000

    fixed indentation

--- jaxws-ri/rt/src/com/sun/xml/ws/spi/runtime/WSConnection.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/spi/runtime/WSConnection.java
@@ -1,5 +1,5 @@
 /**
- * $Id: WSConnection.java,v 1.2 2005-07-15 17:53:18 kohlert Exp $
+ * $Id: WSConnection.java,v 1.3 2005-07-15 22:38:27 kohlert Exp $
  *
  * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -37,8 +37,8 @@ public interface WSConnection extends Connection {
      * INTERNAL_ERR=500
      *
      */
-    enum STATUS { OK, ONEWAY, UNSUPPORTED_MEDIA, MALFORMED_XML,
-    INCORRECT_FORMAT, INTERNAL_ERR };
+    enum STATUS { OK, ONEWAY, UNSUPPORTED_MEDIA, MALFORMED_XML, 
+                  INCORRECT_FORMAT, INTERNAL_ERR };
     
     /**
      * returns request headers. can we use javax.net.http.Headers ?
