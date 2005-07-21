e57adc6cad877adaa7bc501b21f1c7ca2e0e1d2b
Vivek Pandey <vivek.pandey@sun.com>
2005-07-21 02:00:28 +0000

    Exception class generated using JCodeModel.

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/generator/CustomExceptionGenerator.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/generator/CustomExceptionGenerator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CustomExceptionGenerator.java,v 1.2 2005-07-18 18:13:57 kohlert Exp $
+ * $Id: CustomExceptionGenerator.java,v 1.3 2005-07-21 02:00:28 vivekp Exp $
  */
 
 /*
@@ -9,34 +9,24 @@
 
 package com.sun.tools.ws.processor.generator;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Properties;
-import java.util.Set;
-
+import com.sun.codemodel.*;
 import com.sun.tools.ws.processor.config.Configuration;
 import com.sun.tools.ws.processor.model.Fault;
 import com.sun.tools.ws.processor.model.Model;
-import com.sun.tools.ws.processor.model.jaxb.JAXBElementMember;
-import com.sun.tools.ws.processor.model.java.JavaStructureMember;
-import com.sun.tools.ws.processor.model.java.JavaStructureType;
-import com.sun.tools.ws.processor.model.java.JavaStructureMember;
-import com.sun.tools.ws.processor.model.java.JavaStructureType;
-import com.sun.tools.ws.processor.modeler.ModelerConstants;
-import com.sun.tools.ws.processor.util.GeneratedFileInfo;
-import com.sun.tools.ws.processor.util.IndentingWriter;
 import com.sun.xml.ws.encoding.soap.SOAPVersion;
+import com.sun.xml.ws.util.exception.LocalizableExceptionAdapter;
+
+import javax.xml.ws.WebFault;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  *
  * @author WS Development Team
  */
 public class CustomExceptionGenerator extends GeneratorBase20 {
-    private Set faults;
+    private Map<String, JClass> faults = new HashMap<String, JClass>();
 
     public CustomExceptionGenerator() {
     }
@@ -64,7 +54,6 @@ public class CustomExceptionGenerator extends GeneratorBase20 {
     }
 
     protected void preVisitModel(Model model) throws Exception {
-        faults = new HashSet();
     }
 
     protected void postVisitModel(Model model) throws Exception {
@@ -75,230 +64,83 @@ public class CustomExceptionGenerator extends GeneratorBase20 {
         if (isRegistered(fault))
             return;
         registerFault(fault);
-        if (fault.getParentFault() != null) {
-            preVisitFault(fault.getParentFault());
-        }
     }
 
     private boolean isRegistered(Fault fault) {
-        return faults.contains(fault.getJavaException().getName());
+        if(faults.keySet().contains(fault.getJavaException().getName())){
+            fault.setExceptionClass(faults.get(fault.getJavaException().getName()));
+            return true;
+        }
+        return false;
     }
 
     private void registerFault(Fault fault) {
-        faults.add(fault.getJavaException().getName());
-        generateCustomException(fault);
-    }
-
-    private void generateCustomException(Fault fault) {
-        if (fault.getJavaException().isPresent())
-            return;
-        log(
-            "generating CustomException for: "
-                + fault.getJavaException().getName());
-        try {
-            String className = env.getNames().customExceptionClassName(fault);
-            if ((donotOverride && 
-                 GeneratorUtil.classExists(env, className))) {
-                log("Class " + className + " exists. Not overriding.");
-                return;
-            }            
-            File classFile =
-                env.getNames().sourceFileForClass(
-                    className,
-                    className,
-                    sourceDir,
-                    env);
-
-            /* adding the file name and its type */
-            GeneratedFileInfo fi = new GeneratedFileInfo();
-            fi.setFile(classFile);
-            fi.setType(GeneratorConstants.FILE_TYPE_EXCEPTION);
-            env.addGeneratedFile(fi);
-
-            IndentingWriter out =
-                new IndentingWriter(
-                    new OutputStreamWriter(new FileOutputStream(classFile)));
-            writePackage(out, className);
-            out.pln();
-            JavaStructureType javaStructure =
-                (JavaStructureType) fault.getJavaException();
-            writeClassDecl(out, className, javaStructure);
-            writeMembers(out, fault);
-            out.pln();
-            writeClassConstructor(out, className, fault);
-            out.pln();
-            writeGetter(out, fault);
-            out.pOln("}"); // class
-            out.close();
+         try {
+            write(fault);
+            faults.put(fault.getJavaException().getName(), fault.getExceptionClass()); 
         } catch (Exception e) {
-            fail(e);
-        }
-    }
-
-    protected void writeClassDecl(
-        IndentingWriter p,
-        String className,
-        JavaStructureType javaStruct)
-        throws IOException {        
-
-        JAXBElementMember jaxbMember = null;
-        for (JavaStructureMember member : javaStruct.getMembersList())   {
-            if (member.getReadMethod().equals("getFaultInfo") &&
-                member.getOwner() instanceof JAXBElementMember) {
-                jaxbMember = (JAXBElementMember)member.getOwner();
-                break;
-            }
-        }
-        if (jaxbMember == null) {
             throw new GeneratorException(
-                "generator.internal.error.should.not.happen",
-                "stub.generator.003");
-        }
-        p.plnI("@javax.xml.ws.WebFault(name=\""+jaxbMember.getName().getLocalPart()+"\", ");
-        p.pln("targetNamespace=\""+jaxbMember.getName().getNamespaceURI()+"\")");
-        p.pO();        
-        JavaStructureType superclass = javaStruct.getSuperclass();
-        if (superclass != null) {
-            p.plnI(
-                "public class "
-                    + Names.stripQualifier(className)
-                    + " extends "
-                    + superclass.getName()
-                    + " {");
-        } else {
-            p.plnI(
-                "public class "
-                    + Names.stripQualifier(className)
-                    + " extends Exception {");
+                "generator.nestedGeneratorError",
+                new LocalizableExceptionAdapter(e));
         }
     }
 
-    private void writeMembers(IndentingWriter p, Fault fault)
-        throws IOException {
-        Iterator members =
-            ((JavaStructureType) fault.getJavaException()).getMembers();
-        JavaStructureMember member;
-        while (members.hasNext()) {
-            member = (JavaStructureMember) members.next();
-            if (!member.isInherited()) {
-                p.pln(
-                    "private "
-                        + member.getType().getName()
-                        + " "
-                        + member.getName()
-                        + ";");
-            }
+    private void write(Fault fault) throws Exception{
+        String className = env.getNames().customExceptionClassName(fault);
+
+        JDefinedClass cls = cm._class(className, ClassType.CLASS);
+        JDocComment comment = cls.javadoc();
+        for(String doc:getJAXWSClassComment()){
+            comment.add(doc);
         }
-        p.pln();
+        cls._extends(java.lang.Exception.class);
+
+        //@WebFault
+        JAnnotationUse faultAnn = cls.annotate(WebFault.class);
+        faultAnn.param("name", fault.getBlock().getName().getLocalPart());
+        faultAnn.param("targetNamespace", fault.getBlock().getName().getNamespaceURI());
+
+        JType faultBean = fault.getBlock().getType().getJavaType().getType().getType();
+
+        //faultInfo filed
+        JFieldVar fi = cls.field(JMod.PRIVATE, faultBean, "faultInfo");
+
+        //add jaxb annotations
+        fault.getBlock().getType().getJavaType().getType().annotate(fi);
+
+        fi.javadoc().add("Java type that goes as soapenv:Fault detail element.");
+        JFieldRef fr = JExpr.ref(JExpr._this(), fi);
+
+        //Constructor
+        JMethod constrc1 = cls.constructor(JMod.PUBLIC);
+        JVar var1 = constrc1.param(String.class, "message");
+        JVar var2 = constrc1.param(faultBean, "faultInfo");
+        constrc1.javadoc().addParam(var1);
+        constrc1.javadoc().addParam(var2);
+        JBlock cb1 = constrc1.body();
+        cb1.invoke("super").arg(var1);
+
+        cb1.assign(fr, var2);
+
+        //constructor with Throwable
+        JMethod constrc2 = cls.constructor(JMod.PUBLIC);
+        var1 = constrc2.param(String.class, "message");
+        var2 = constrc2.param(faultBean, "faultInfo");
+        JVar var3 = constrc2.param(Throwable.class, "cause");
+        constrc2.javadoc().addParam(var1);
+        constrc2.javadoc().addParam(var2);
+        constrc2.javadoc().addParam(var3);
+        JBlock cb2 = constrc2.body();
+        cb2.invoke("super").arg(var1).arg(var3);
+        cb2.assign(fr, var2);
+
+
+        //getFaultInfo() method
+        JMethod fim = cls.method(JMod.PUBLIC, faultBean, "getFaultInfo");
+        fim.javadoc().addReturn().add("returns fault bean: "+faultBean.fullName());
+        JBlock fib = fim.body();
+        fib._return(fi);
+        fault.setExceptionClass(cls);
+
     }
-
-    protected void writeClassConstructor(
-        IndentingWriter p,
-        String className,
-        Fault fault)
-        throws IOException {
-        JavaStructureType javaStructure =
-            (JavaStructureType) fault.getJavaException();
-
-        //WrapperException(String message, FaultBean faultInfo)
-        p.p("public " + Names.stripQualifier(className) + "(String message, ");
-        Iterator members = javaStructure.getMembers();
-        JavaStructureMember member;
-        for (int i = 0; members.hasNext(); i++) {
-            member = (JavaStructureMember) members.next();
-            if (i > 0)
-                p.p(", ");
-            p.p(member.getType().getName() + " " + member.getName());
-        }
-        p.plnI(") {");
-        if (fault.getParentFault() != null) {
-            members = javaStructure.getMembers();
-            int i = 0;
-            while (members.hasNext()) {
-                member = (JavaStructureMember) members.next();
-                if (member.isInherited()) {
-                    if (i++ == 0)
-                        p.p("super(message, ");
-                    else
-                        p.p(", ");
-                    p.p(member.getName());
-                }
-            }
-            if (i > 0)
-                p.pln(");");
-        } else{
-            p.pln("super(message);");
-        }
-        members = fault.getJavaException().getMembers();
-        for (int i = 0; members.hasNext(); i++) {
-            member = (JavaStructureMember) members.next();
-            if (!member.isInherited()) {
-                p.pln("this." + member.getName() + " = " + member.getName() + ";");
-            }
-        }
-        p.pOln("}");
-        p.pln();
-
-        //WrapperException(String message, FaultBean faultInfo, Throwable cause)
-        p.p("public " + Names.stripQualifier(className) + "(String message, ");
-        members = javaStructure.getMembers();
-        for (int i = 0; members.hasNext(); i++) {
-            member = (JavaStructureMember) members.next();
-            if (i > 0)
-                p.p(", ");
-            p.p(member.getType().getName() + " " + member.getName());
-        }
-        p.plnI(", Throwable cause) {");
-        if (fault.getParentFault() != null) {
-            members = javaStructure.getMembers();
-            int i = 0;
-            while (members.hasNext()) {
-                member = (JavaStructureMember) members.next();
-                if (member.isInherited()) {
-                    if (i++ == 0)
-                        p.p("super(message, ");
-                    else
-                        p.p(", ");
-                    p.p(member.getName());
-                }
-            }
-            if (i > 0)
-                p.pln(", cause);");
-        } else{
-            p.pln("super(message, cause);");
-        }
-        members = fault.getJavaException().getMembers();
-        for (int i = 0; members.hasNext(); i++) {
-            member = (JavaStructureMember) members.next();
-            if (!member.isInherited()) {
-                p.pln("this." + member.getName() + " = " + member.getName() + ";");
-            }
-        }
-        p.pOln("}");
-    }
-
-    private void writeGetter(IndentingWriter p, Fault fault)
-        throws IOException {
-        Iterator members =
-            ((JavaStructureType) fault.getJavaException()).getMembers();
-        JavaStructureMember member;
-        int i = 0;
-        while (members.hasNext()) {
-            if (i > 0)
-                p.pln();
-            member = (JavaStructureMember) members.next();
-            if (!member.isInherited()) {
-                p.plnI(
-                    "public "
-                        + member.getType().getName()
-                        + " "
-                        + member.getReadMethod()
-                        + "() {");
-                p.pln("return " + member.getName() + ";");
-                p.pOln("}");
-                i++;
-            }
-        }
-    }
-
 }
