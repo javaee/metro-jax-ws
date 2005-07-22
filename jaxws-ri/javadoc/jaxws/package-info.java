cc5f56205c05b21473539c213e3a2831f229954d
Doug Kohlert <doug.kohlert@sun.com>
2005-07-22 01:35:01 +0000

    more files to aid in architecture document generation

---
+++ jaxws-ri/javadoc/jaxws/package-info.java
@@ -0,0 +1,83 @@
+/**
+ * <h1>JAX-WS RI Architecture Document</h1>.
+ *
+ * <h2>JAX-WS RI Major Modules and Libraries</h2>
+ * {@DotDiagram
+     digraph G {
+       // 
+       APT; "Java EE";
+ 
+       // libraries
+       node [style=filled,color=lightblue];
+       "JAXB-RT-API" [label="JAXB runtime API"]; "JAXB XJC API";  SAAJ; 
+
+       // modules
+       node [style=filled,color=lightpink];
+       "JAX-WS"; tools; runtime; SPI; "Annotation Processor";
+
+       "JAX-WS" -> tools;
+       "JAX-WS" -> runtime;
+       "JAX-WS" -> SPI;
+       "JAX-WS" -> "Annotation Processor";
+       APT -> "Annotation Processor" -> tools -> APT;
+       tools -> "JAXB XJC API";
+       runtime -> SAAJ;
+       runtime -> "JAXB-RT-API";
+       SPI -> tools;
+       SPI -> runtime;
+       "Java EE" -> SPI;
+     }
+ * }
+ * <div align=right>
+ * <b>Legend:</b> blue: external library, pink: module, white: external tool
+ * </div>
+ * 
+ *
+ * <h2>Modules</h2>
+ * <p>
+ * The JAX-WS RI consists of the following major modules.
+ *
+ * <dl>
+ *  <dt>{@link com.sun.xml.ws runtime}
+ *  <dd>
+ *    runtime module is available at application runtime and provide the actual
+ *    core Web Services framework.
+ *
+ *  <dt>{@link com.sun.tools.ws tools}
+ *  <dd>
+ *    Tools for converting WSDLs and Java source/class files to Web Services.
+ *
+ *  <dt><a href="http://java.sun.com/j2se/1.5.0/docs/tooldocs/share/apt.html">APT</a>
+ *  <dd>A Java SE tool and framework for processing annotations.
+ *
+ *  <dt>{@link com.sun.istack.ws Annotation Processor}
+ *  <dd>
+ *    An APT AnnotationProcossor for processing JavaSource files with javax.jws.* annotations and making them web services.
+ *
+ *  <dt>{@link com.sun.tools.ws.spi SPI}
+ *  <dd>
+ *    A part of JAX-WS that defines the contract between the JAX-WS RI and
+ *    Java EE.
+ *
+ *  <dt>{@link com.sun.tools.xjc.api JAXB XJC-API}
+ *  <dd>
+ *    The schema compiler.
+ *
+ *  <dt>{@link com.sun.xml.bind.api JAXB runtime-API}</a>
+ *  <dd>
+ *    A part of the JAXB runtime that defines the contract between the JAXB RI and
+ *    the JAX-WS RI.
+ *
+ * </dl>
+ *
+ * <h2>About This Document</h2>
+ * <p>
+ * See {@link jaxb.MetaArchitectureDocument} for how to contribute to this document.  Althought this document refers to 
+ *      JAXB, JAX-WS is using the same mechanism.
+ *
+ * @ArchitectureDocument
+ */
+package jaxws;
+
+import javax.xml.ws.Binding;
+
