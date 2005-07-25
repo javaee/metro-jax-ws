d41a1d50926b6b27cf2022c93b85c9ebb461c69c
Ken Walsh <ken.walsh@sun.com>
2005-07-25 15:58:52 +0000

    SOAPConnectionUtil relied on RuntimeContext to get BindingId- Since Dispact has not RuntimeContext and RuntimeModel associated to it this method threw NPE . Fixed this block of to execute only if RuntimeContext is not null

--- jaxws-ri/rt/src/com/sun/xml/ws/util/SOAPConnectionUtil.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/SOAPConnectionUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SOAPConnectionUtil.java,v 1.5 2005-07-22 23:04:30 arungupta Exp $
+ * $Id: SOAPConnectionUtil.java,v 1.6 2005-07-25 15:58:52 kwalsh Exp $
  */
 
 /*
@@ -9,64 +9,63 @@
 
 package com.sun.xml.ws.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import com.sun.pept.ept.MessageInfo;
+import com.sun.xml.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.ws.binding.soap.BindingImpl;
+import com.sun.xml.ws.server.RuntimeContext;
+import com.sun.xml.ws.server.RuntimeEndpointInfo;
+import com.sun.xml.ws.server.SOAPConnection;
+import com.sun.xml.ws.spi.runtime.WSConnection;
+import com.sun.xml.ws.spi.runtime.WSConnection.STATUS;
 
-import javax.xml.ws.WebServiceException;
 import javax.xml.soap.MimeHeader;
 import javax.xml.soap.MimeHeaders;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.stream.StreamSource;
-
-import com.sun.xml.messaging.saaj.util.ByteInputStream;
-import com.sun.xml.ws.spi.runtime.WSConnection;
-import com.sun.xml.ws.spi.runtime.WSConnection.STATUS;
-import com.sun.xml.ws.binding.soap.BindingImpl;
-import com.sun.pept.ept.MessageInfo;
-import com.sun.xml.ws.server.*;
+import javax.xml.ws.WebServiceException;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 /**
  * @author WS Development Team
  */
 public class SOAPConnectionUtil {
-    
+
     public static SOAPMessage getSOAPMessage(WSConnection con, MessageInfo mi, String bindingId) {
         if (con instanceof SOAPConnection) {
-            return ((SOAPConnection)con).getSOAPMessage();
+            return ((SOAPConnection) con).getSOAPMessage();
         }
         try {
             Map<String, List<String>> headers = con.getHeaders();
             MimeHeaders mh = new MimeHeaders();
             if (headers != null)
-                for(Map.Entry<String, List<String>> entry : headers.entrySet()) {
+                for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
                     String name = entry.getKey();
-                    for(String value : entry.getValue()) {
+                    for (String value : entry.getValue()) {
                         mh.addHeader(name, value);
                     }
                 }
             RuntimeContext rtCtxt = MessageInfoUtil.getRuntimeContext(mi);
-            RuntimeEndpointInfo endpointInfo = rtCtxt.getRuntimeEndpointInfo();
-            if (bindingId == null)
-                bindingId = ((BindingImpl)endpointInfo.getBinding()).getBindingId();
-            SOAPMessage soapMessage =  SOAPUtil.createMessage(mh,
-                    con.getInput(), bindingId);
+            if (rtCtxt != null) {
+                RuntimeEndpointInfo endpointInfo = rtCtxt.getRuntimeEndpointInfo();
+                if (bindingId == null)
+                    bindingId = ((BindingImpl) endpointInfo.getBinding()).getBindingId();
+            }
+            SOAPMessage soapMessage = SOAPUtil.createMessage(mh,
+                con.getInput(), bindingId);
             return soapMessage;
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new WebServiceException(e);
         }
     }
-    
+
     private static void send(WSConnection con, SOAPMessage soapMessage) {
         if (con instanceof SOAPConnection) {
-            ((SOAPConnection)con).sendResponse(soapMessage);
+            ((SOAPConnection) con).sendResponse(soapMessage);
             return;
         }
         try {
@@ -76,8 +75,8 @@ public class SOAPConnectionUtil {
             Map<String, List<String>> headers = new HashMap<String, List<String>>();
             MimeHeaders mhs = soapMessage.getMimeHeaders();
             Iterator i = mhs.getAllHeaders();
-            while(i.hasNext()) {
-                MimeHeader mh = (MimeHeader)i.next();
+            while (i.hasNext()) {
+                MimeHeader mh = (MimeHeader) i.next();
                 String name = mh.getName();
                 List<String> values = headers.get(name);
                 if (values == null) {
@@ -90,26 +89,26 @@ public class SOAPConnectionUtil {
             con.setHeaders(headers);
             soapMessage.writeTo(con.getOutput());
             con.closeOutput();
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new WebServiceException(e);
         }
     }
-    
+
     public static void sendResponse(WSConnection con, SOAPMessage soapMessage) {
         setStatus(con, STATUS.OK);
         send(con, soapMessage);
     }
-    
+
     public static void sendResponseOneway(WSConnection con) {
-         if (con instanceof SOAPConnection) {
-            ((SOAPConnection)con).sendResponseOneway();
+        if (con instanceof SOAPConnection) {
+            ((SOAPConnection) con).sendResponseOneway();
             return;
         }
         ByteBuffer buf = ByteBuffer.wrap(new byte[0]);
         setStatus(con, STATUS.ONEWAY);
         //con.write(buf);
     }
-        
+
     public static void sendResponseError(WSConnection con) {
         try {
             SOAPMessage message = SOAPUtil.createMessage();
@@ -118,31 +117,30 @@ public class SOAPConnectionUtil {
             writer.write(DEFAULT_SERVER_ERROR_ENVELOPE);
             writer.close();
             byte[] data = bufferedStream.toByteArray();
-            message.getSOAPPart().setContent(
-                new StreamSource(new ByteInputStream(data, data.length)));
+            message.getSOAPPart().setContent(new StreamSource(new ByteInputStream(data, data.length)));
             setStatus(con, STATUS.INTERNAL_ERR);
             send(con, message);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new WebServiceException(e);
         }
     }
-    
-    public static Map<String,List<String>> getHeaders(WSConnection con) {
+
+    public static Map<String, List<String>> getHeaders(WSConnection con) {
         return con.getHeaders();
     }
-    
+
     /**
      * sets response headers.
      */
     public static void setHeaders(WSConnection con,
-        Map<String,List<String>> headers) {
+                                  Map<String, List<String>> headers) {
         con.setHeaders(headers);
     }
-    
+
     public static void setStatus(WSConnection con, STATUS status) {
         con.setStatus(status);
     }
-    
+
     private final static String DEFAULT_SERVER_ERROR_ENVELOPE =
         "<?xml version='1.0' encoding='UTF-8'?>"
         + "<env:Envelope xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\">"
