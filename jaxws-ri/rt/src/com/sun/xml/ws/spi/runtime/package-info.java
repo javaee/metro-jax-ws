261556e59c559697eb72cc4b6f985c583fdacbfc
Doug Kohlert <doug.kohlert@sun.com>
2005-08-02 16:52:10 +0000

    part of the architecture document

---
+++ jaxws-ri/rt/src/com/sun/xml/ws/spi/runtime/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * <P>This document describes the SPI for the JAX-WS 2.0 runtime.
+ *  Applications can use this SPI to interact with the runtime at
+ *  various points to get access to information and to perform 
+ *  various operations such as security etc.
+*/
+package com.sun.xml.ws.spi.runtime;
