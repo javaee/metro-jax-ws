c6bab182d7459fc83b167925e0b6dc3c18742cd5
Doug Kohlert <doug.kohlert@sun.com>
2005-08-08 17:19:52 +0000

    removed commented out code and code not used

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/modeler/annotation/ModelBuilder.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/modeler/annotation/ModelBuilder.java
@@ -1,5 +1,5 @@
 /**
- * $Id: ModelBuilder.java,v 1.2 2005-07-18 18:14:03 kohlert Exp $
+ * $Id: ModelBuilder.java,v 1.3 2005-08-08 17:19:52 kohlert Exp $
  *
  * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -29,38 +29,23 @@ import com.sun.tools.ws.processor.modeler.annotation.*;
  * @author WS Development Team
  */
 public interface ModelBuilder {
-//    public ClassLoader getClassLoader();
     public AnnotationProcessorEnvironment getAPEnv();
     public void createModel(TypeDeclaration d, QName modelName, String targetNamespace, String modelerClassName);
     public void setService(Service service);
     public void setPort(Port port);
-//    public String getWsdlNamespaceURI();
-//    public void setWsdlNamespaceURI(String namespace);
     public String getOperationName(String methodName);
     public String getResponseName(String operationName);
     public TypeMirror getHolderValueType(TypeMirror type);
-//    public String getParamName(TypeMirror type, int pos);
-//    public Properties getOptions();
     public boolean checkAndSetProcessed(TypeDeclaration typeDecl);
-//    public URL findResource(String resourceName);
     public boolean isRemoteException(TypeDeclaration typeDecl);
     public boolean isRemote(TypeDeclaration typeDecl);
     public boolean canOverWriteClass(String className);
     public void setWrapperGenerated(boolean wrapperGenerated);
-    public void setPortProperties(Port port);
     public TypeDeclaration getTypeDeclaration(String typeName);
-//    public DeclaredType getDeclaredType(TypeDeclaration typeDecl);
-//    public TypeMirror getTypeMirror(String typeName);
     public String getVersionString();
     public ProcessorEnvironment getProcessorEnvironment();
     public File getSourceDir();
     public String getXMLName(String javaName);
-//    public QName getWSDLPortName(String portName);
-//    public QName getWSDLBindingName(String portName);
-//    public QName getWSDLPortTypeName(String portName);
-//    public QName getWSDLInputMessageName(String operationName);
-//    public QName getWSDLOutputMessageName(String operationName);
- //   public QName getWSDLFaultMessageName(String faultName);
     public void onError(String key);
     public void onError(String key, Object[] args) throws ModelerException;
     public void onError(Localizable msg) throws ModelerException;
