4f02c2a7a34fc77a04a5d2d5bdcb93b55f27e4e6
Doug Kohlert <doug.kohlert@sun.com>
2005-08-08 16:55:08 +0000

    removed unused fields

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/model/jaxb/JAXBStructuredType.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/processor/model/jaxb/JAXBStructuredType.java
@@ -1,5 +1,5 @@
 /**
- * $Id: JAXBStructuredType.java,v 1.2 2005-07-23 04:10:58 kohlert Exp $
+ * $Id: JAXBStructuredType.java,v 1.3 2005-08-08 16:55:08 kohlert Exp $
  *
  * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -119,14 +119,8 @@ public class JAXBStructuredType extends JAXBType {
     }
 
 
-
     private List _elementMembers = new ArrayList();
     private Map _elementMembersByName = new HashMap();
-    //private LiteralContentMember _contentMember;
     private Set _subtypes = null;
     private JAXBStructuredType _parentType = null;
-    private boolean _rpcWrapper = false;
-
-
-
 }
