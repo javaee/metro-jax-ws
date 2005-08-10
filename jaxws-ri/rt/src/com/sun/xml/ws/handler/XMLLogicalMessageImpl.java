d33790a32ce90536c8841e2ebaefb17e9a273037
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2005-08-10 01:51:29 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    Removed unnecessary imports.

--- jaxws-ri/rt/src/com/sun/xml/ws/handler/XMLLogicalMessageImpl.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/handler/XMLLogicalMessageImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: XMLLogicalMessageImpl.java,v 1.3 2005-08-10 01:36:45 jitu Exp $
+ * $Id: XMLLogicalMessageImpl.java,v 1.4 2005-08-10 01:51:29 jitu Exp $
  *
  * Copyright (c) 2005 Sun Microsystems, Inc.
  * All rights reserved.
@@ -7,22 +7,8 @@
 package com.sun.xml.ws.handler;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.ws.WebServiceException;
 import javax.xml.ws.LogicalMessage;
 import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.dom.DOMSource;
-
-import com.sun.pept.ept.MessageInfo;
-import com.sun.xml.ws.encoding.jaxb.JAXBBeanInfo;
-import com.sun.xml.ws.encoding.jaxb.LogicalEPTFactory;
-import com.sun.xml.ws.encoding.jaxb.LogicalEncoder;
-import com.sun.xml.ws.encoding.soap.internal.BodyBlock;
-import com.sun.xml.ws.encoding.soap.internal.InternalMessage;
-import com.sun.xml.ws.util.xml.XmlUtil;
-import com.sun.xml.ws.encoding.xml.XMLEPTFactory;
 import com.sun.xml.ws.encoding.xml.XMLMessage;
 
 /**
@@ -46,7 +32,8 @@ public class XMLLogicalMessageImpl implements LogicalMessage {
     }
 
     /*
-     *
+     * Gets the source from XMLMessage. XMLMessage gives a copy of existing
+     * data
      */
     public Source getPayload() {
         XMLMessage xmlMessage = ctxt.getXMLMessage();
@@ -62,7 +49,7 @@ public class XMLLogicalMessageImpl implements LogicalMessage {
     }
 
     /*
-     *
+     * Gets XMLMessage data as JAXB bean
      */
     public Object getPayload(JAXBContext jaxbContext) {
         XMLMessage xmlMessage = ctxt.getXMLMessage();
@@ -70,7 +57,7 @@ public class XMLLogicalMessageImpl implements LogicalMessage {
     }
 
     /*
-     *
+     * Sets JAXB bean into XMLMessage
      */
     public void setPayload(Object bean, JAXBContext jaxbContext) {
         XMLMessage xmlMessage = ctxt.getXMLMessage();
