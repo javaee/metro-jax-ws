2d285e6999ed4381dcb56702d94f706f3e563c14
Arun Gupta <arun.p.gupta@oracle.com>
2005-10-14 20:23:05 +0000

    Fixed 6233519
    Added property validation when adding properties to SOAPMessageContext
    
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- jaxws-ri/rt/src/com/sun/xml/ws/handler/SOAPMessageContextImpl.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/handler/SOAPMessageContextImpl.java
@@ -23,6 +23,12 @@ import com.sun.xml.ws.encoding.jaxb.JAXBBeanInfo;
 import com.sun.xml.ws.encoding.jaxb.JAXBTypeSerializer;
 import com.sun.xml.ws.encoding.soap.SOAPEPTFactory;
 import com.sun.xml.ws.encoding.soap.internal.InternalMessage;
+import java.util.Collections;
+import java.util.HashSet;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.namespace.QName;
@@ -33,6 +39,7 @@ import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.WebServiceException;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
@@ -60,20 +67,65 @@ public class SOAPMessageContextImpl implements SOAPMessageContext {
     private Set<URI> roles;
     private static Map<String, Class> allowedTypes = null;
     private boolean failure;
+    private static final Set<String> KNOWN_PROPERTIES;
+    
+    static {
+        allowedTypes = new HashMap<String, Class>();
+        allowedTypes.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, String.class);
+        allowedTypes.put(BindingProvider.PASSWORD_PROPERTY, String.class);
+        allowedTypes.put(BindingProvider.SESSION_MAINTAIN_PROPERTY, Boolean.class);
+        allowedTypes.put(BindingProvider.SOAPACTION_URI_PROPERTY, String.class);
+        allowedTypes.put(BindingProvider.SOAPACTION_USE_PROPERTY, Boolean.class);
+        allowedTypes.put(BindingProvider.USERNAME_PROPERTY, String.class);
+        
+        allowedTypes.put(MessageContext.HTTP_REQUEST_HEADERS, Map.class);
+        allowedTypes.put(MessageContext.HTTP_REQUEST_METHOD, String.class);
+        allowedTypes.put(MessageContext.HTTP_RESPONSE_CODE, Integer.class);
+        allowedTypes.put(MessageContext.HTTP_RESPONSE_HEADERS, Map.class);
+        allowedTypes.put(MessageContext.MESSAGE_ATTACHMENTS, Map.class);
+        allowedTypes.put(MessageContext.MESSAGE_OUTBOUND_PROPERTY, Boolean.class);
+        allowedTypes.put(MessageContext.SERVLET_CONTEXT, ServletContext.class);
+        allowedTypes.put(MessageContext.SERVLET_REQUEST, HttpServletRequest.class);
+        allowedTypes.put(MessageContext.SERVLET_RESPONSE, HttpServletResponse.class);
+        allowedTypes.put(MessageContext.SERVLET_SESSION, HttpSession.class);
+        allowedTypes.put(MessageContext.WSDL_DESCRIPTION, URI.class);
+        allowedTypes.put(MessageContext.WSDL_SERVICE, QName.class);
+        allowedTypes.put(MessageContext.WSDL_PORT, QName.class);
+        allowedTypes.put(MessageContext.WSDL_INTERFACE, QName.class);
+        allowedTypes.put(MessageContext.WSDL_OPERATION, QName.class);
+        allowedTypes.put(MessageContext.MESSAGE_OUTBOUND_PROPERTY, Boolean.class);
+        
+        Set<String> temp = new HashSet<String>();
+        temp.add(BindingProvider.ENDPOINT_ADDRESS_PROPERTY);
+        temp.add(BindingProvider.PASSWORD_PROPERTY);
+        temp.add(BindingProvider.SESSION_MAINTAIN_PROPERTY);
+        temp.add(BindingProvider.SOAPACTION_URI_PROPERTY);
+        temp.add(BindingProvider.SOAPACTION_USE_PROPERTY);
+        temp.add(BindingProvider.USERNAME_PROPERTY);
+        
+        temp.add(MessageContext.HTTP_REQUEST_HEADERS);
+        temp.add(MessageContext.HTTP_REQUEST_METHOD);
+        temp.add(MessageContext.HTTP_RESPONSE_CODE);
+        temp.add(MessageContext.HTTP_RESPONSE_HEADERS);
+        temp.add(MessageContext.MESSAGE_ATTACHMENTS);
+        temp.add(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        temp.add(MessageContext.SERVLET_CONTEXT);
+        temp.add(MessageContext.SERVLET_REQUEST);
+        temp.add(MessageContext.SERVLET_RESPONSE);
+        temp.add(MessageContext.SERVLET_SESSION);
+        temp.add(MessageContext.WSDL_DESCRIPTION);
+        temp.add(MessageContext.WSDL_SERVICE);
+        temp.add(MessageContext.WSDL_PORT);
+        temp.add(MessageContext.WSDL_INTERFACE);
+        temp.add(MessageContext.WSDL_OPERATION);
+        KNOWN_PROPERTIES = Collections.unmodifiableSet(temp);
+    }
 
     public SOAPMessageContextImpl(SOAPHandlerContext handlerCtxt) {
         this.handlerCtxt = handlerCtxt;
         this.ctxt = handlerCtxt.getMessageContext();
-        if (allowedTypes == null) {
-            allowedTypes = new HashMap<String, Class>();
-            allowedTypes.put(MessageContext.MESSAGE_ATTACHMENTS, Map.class);
-            allowedTypes.put(MessageContext.WSDL_DESCRIPTION, URI.class);
-            allowedTypes.put(MessageContext.WSDL_SERVICE, QName.class);
-            allowedTypes.put(MessageContext.WSDL_PORT, QName.class);
-            allowedTypes.put(MessageContext.WSDL_INTERFACE, QName.class);
-            allowedTypes.put(MessageContext.WSDL_OPERATION, QName.class);
-            allowedTypes.put(MessageContext.MESSAGE_OUTBOUND_PROPERTY, Boolean.class);
-        }
+//        if (allowedTypes == null) {
+//        }
     }
 
     public SOAPMessage getMessage() {
@@ -139,7 +191,11 @@ public class SOAPMessageContextImpl implements SOAPMessageContext {
         this.roles = roles;
     }
 
-    private boolean validateProperty(String name, Object value) {
+    private void validateProperty(String name, Object value) {
+        if (name == null)
+            throw new WebServiceException(name + " is a User-defined property - property name can not be null. ",
+                new IllegalArgumentException("Name of property is null.  This is an invalid property name. "));
+
         if (allowedTypes.containsKey(name)) {
             Class clazz = allowedTypes.get(name);
             if (!(clazz.isInstance(value)))
@@ -147,7 +203,12 @@ public class SOAPMessageContextImpl implements SOAPMessageContext {
                         new Object[] { value, name });
         }
 
-        return true;
+        if (!KNOWN_PROPERTIES.contains(name)) {
+            //do validation check on not "javax.xml.ws."
+            if (name.startsWith("javax.xml.ws"))
+                throw new WebServiceException(name + " is a User-defined property - can not start with javax.xml.ws. package",
+                    new IllegalArgumentException("can not start with javax.xml.ws. package"));                                            //let's check the propertyContext
+        }
     }
 
     public void setScope(String name, Scope scope) {
@@ -189,10 +250,14 @@ public class SOAPMessageContextImpl implements SOAPMessageContext {
     }
 
     public Object put(String str, Object obj) {
+        validateProperty(str, obj);
         return ctxt.put(str, obj);
     }
 
     public void putAll(Map<? extends String, ? extends Object> map) {
+        for (String key : map.keySet()) {
+            validateProperty(key, map.get(key));
+        }
         ctxt.putAll(map);
     }
 
