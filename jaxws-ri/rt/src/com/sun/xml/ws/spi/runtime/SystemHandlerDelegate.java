8ed004f226912915a1f2bf8278875b9dafedefa8
Ken Walsh <ken.walsh@sun.com>
2005-10-25 14:16:00 +0000

    6329361- updated javadoc

--- jaxws-ri/rt/src/com/sun/xml/ws/spi/runtime/SystemHandlerDelegate.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/spi/runtime/SystemHandlerDelegate.java
@@ -21,6 +21,10 @@ package com.sun.xml.ws.spi.runtime;
 
 
 /**
+ * A SystemHandlerDelegate is used to inject system level functionality into a 
+ * message processing runtime. The methods of this interface are invoked by 
+ * the client and enpoint message dispatchers of the message processing
+ * runtime.
  *
  * @author WS Development Team
  */
@@ -28,21 +32,78 @@ package com.sun.xml.ws.spi.runtime;
 public interface SystemHandlerDelegate {
 
    /**
-    * This method is called before MU processing. 
+    * Called by both client and endpoint message dispatchers to activate
+    * injected request message processing.
+    * When called by a client side message dispatcher, this method must be 
+    * called just before the message (associated with the MessageContext) 
+    * is sent. When called by the message dispatcher at an endpoint, this 
+    * method must be called before MustUnderstand processing on the 
+    * associated message.
     *
-    * Invoker object can be got from MessageContextUtil.getInvoker(MessageContext)
+    * @param messageContext when called by a SOAPBinding the argument
+    * must be an instanceof com.sun.xml.ws.spi.runtime.SOAPMessageContext, and
+    * when called by a SOAPBinding at an endpoint, the argument must
+    * be an instanceof com.sun.xml.ws.spi.runtime.SOAPMessageContext and the 
+    * Invoker (on the context) must be available for use on the server by the 
+    * delegate. An argument SOAPMessageContext passed to this method by an endpoint
+    * dispatcher, must have values assigned for the following MessageContext 
+    * properties.
+    * <ul>
+    * <li>MessageContext.SERVLET_REQUEST
+    * <li>MessageContext.SERVLET_RESPONSE
+    * <li>MessageContext.SERVLET_SESSION
+    * <li>MessageContext.SERVLET_CONTEXT
+    * </ul>
+    * @return true if processing by the delegate was such that the caller
+    * should continue with its normal message processing. Returns false when 
+    * the delegate has established, in the MessageContext, 
+    * the response message to be sent. When this method returns 
+    * false, the calling message dispatcher must return the response message 
+    * without performing MustUnderstand processing and without invoking the 
+    * endpoint. Only delegates called by endpoint side message dispatchers 
+    * may return false
+    *
+    * @throws java.lang.Exception when the processing by the delegate failed
+    * without yielding a response message; in which case, the caller shall
+    * determine how to process the error.
+    *
     */
     public boolean processRequest(MessageContext messageContext) throws Exception;
 
    /**
-    * This method is called after Invoker.invoke().
+    * Called by both client and endpoint message dispatchers to activate
+    * injected response message processing.
+    * When called by the message dispatcher at the client, this method must be 
+    * called before MustUnderstand processing on the received message 
+    * (associated with the MessageContext). When called by the message 
+    * dispatcher at an endpoint, this method must be called after the 
+    * endpoint has been invoked, and just before the associated response 
+    * message is sent. In the special case where invocation of the endpoint
+    * caused an Exception to be thrown, this method must not be called.
+    *
+    * @param messageContext when called by a SOAPBinding the argument
+    * must be an instanceof com.sun.xml.ws.spi.runtime.SOAPMessageContext.
+    *
+    * @throws java.lang.Exception when the processing by the delegate failed.
+    * In this case, the caller must not send the response message but shall 
+    * otherwise determine how to process the error.
     */
     public void processResponse(MessageContext messageContext) throws Exception;
     
-    /**
-     * Called before the method invocation.
-     * @param messageContext contains property bag with the scope of the
-     *                       properties
+   /**
+    * This method must be called by an endpoint message dispatcher after 
+    * MustUnderstand processing and before endpoint invocation.
+    *
+    * @param messageContext when called by a SOAPBinding the argument
+    * must be an instanceof com.sun.xml.ws.spi.runtime.SOAPMessageContext, and
+    * must have values assigned for the following MessageContext 
+    * properties.
+    * <ul>
+    * <li>MessageContext.SERVLET_REQUEST
+    * <li>MessageContext.SERVLET_RESPONSE
+    * <li>MessageContext.SERVLET_SESSION
+    * <li>MessageContext.SERVLET_CONTEXT
+    * </ul>
      */
     public void preInvokeEndpointHook(MessageContext messageContext);
 }
