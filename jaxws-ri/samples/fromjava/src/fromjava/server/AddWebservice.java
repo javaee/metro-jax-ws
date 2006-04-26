596db533dc8fde15d8684bdc2ba99b945267a718
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2006-04-26 19:36:34 +0000

    Issue number: Obtained from: Submitted by: Reviewed by: Adding a way to shutdown SE endpoint

--- jaxws-ri/samples/fromjava/src/fromjava/server/AddWebservice.java
+++ jaxws-ri/samples/fromjava/src/fromjava/server/AddWebservice.java
@@ -30,24 +30,11 @@ import javax.xml.ws.Endpoint;
 public class AddWebservice {
     
     public static void main (String[] args) throws Exception {
-        Endpoint.publish (
+        Endpoint endpoint = Endpoint.publish (
             "http://localhost:8080/jaxws-fromjava/addnumbers",
             new AddNumbersImpl ());
+
+        // Stops the endpoint if it receives request http://localhost:9090/stop
+        new EndpointStopper(9090, endpoint);
     }
-    
-//    public static void deployMethod2 () throws Exception {
-//        Endpoint endpoint = Endpoint.create(
-//            new URI (SOAPBinding.SOAP11HTTP_BINDING),
-//            new AddNumbersImpl ());
-//        
-//        HttpServer server = HttpServer.create (new InetSocketAddress (8080), 5);
-//        server.setExecutor (Executors.newFixedThreadPool (5));
-//        HttpContext context = server.createContext (
-//            "http",
-//            "/jaxws-fromjava/addnumbers");
-//        
-//        endpoint.publish (context);
-//        server.start ();
-//    }
-    
 }
