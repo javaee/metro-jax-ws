ff65e5f45313e4e1e017c644bc814827aed188b4
Paul Sandoz <paul.sandoz@oracle.com>
2006-06-16 11:30:36 +0000

    Reverted FI runtime support. Standard class loader is used. FI class names will be renamed for delivery into j2se.

--- jaxws-ri/rt/src/com/sun/xml/ws/util/FastInfosetReflection.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/FastInfosetReflection.java
@@ -134,17 +134,8 @@ public class FastInfosetReflection {
     static {
         // Use reflection to avoid static dependency with FI jar
         try {
-            // Load the FastInfosetRuntime
-            Class clazz = Class.forName("com.sun.fastinfoset.runtime.FastInfosetRuntime");
-            Method m = clazz.getDeclaredMethod("getClassLoader", 
-                    new Class[] { ClassLoader.class });
-            // Obtain the class loader to use to load the rest of the Fast Infoset classes
-            ClassLoader cl = (ClassLoader)m.invoke(null, 
-                    new Object[] { FastInfosetReflection.class.getClassLoader() });
-
-            
-            clazz = Class.forName("com.sun.xml.fastinfoset.stax.StAXDocumentParser",
-                    true, cl);
+            Class clazz = null;
+            clazz = Class.forName("com.sun.xml.fastinfoset.stax.StAXDocumentParser");
             fiStAXDocumentParser_new = clazz.getConstructor();
             fiStAXDocumentParser_setInputStream =
                 clazz.getMethod("setInputStream", java.io.InputStream.class);
@@ -152,8 +143,7 @@ public class FastInfosetReflection {
                 clazz.getMethod("setStringInterning", boolean.class);
 
             clazz =
-                Class.forName("com.sun.xml.fastinfoset.stax.StAXDocumentSerializer",
-                    true, cl);
+                Class.forName("com.sun.xml.fastinfoset.stax.StAXDocumentSerializer");
             fiStAXDocumentSerializer_new = clazz.getConstructor();
             fiStAXDocumentSerializer_setOutputStream =
                 clazz.getMethod("setOutputStream", java.io.OutputStream.class);
@@ -161,30 +151,26 @@ public class FastInfosetReflection {
                 clazz.getMethod("setEncoding", String.class);
             
             clazz =
-                Class.forName("com.sun.xml.fastinfoset.dom.DOMDocumentParser",
-                    true, cl);
+                Class.forName("com.sun.xml.fastinfoset.dom.DOMDocumentParser");
             fiDOMDocumentParser_new = clazz.getConstructor();
             fiDOMDocumentParser_parse = clazz.getMethod("parse", 
                 new Class[] { org.w3c.dom.Document.class, java.io.InputStream.class });
             
-            clazz = Class.forName("com.sun.xml.fastinfoset.dom.DOMDocumentSerializer",
-                    true, cl);
+            clazz = Class.forName("com.sun.xml.fastinfoset.dom.DOMDocumentSerializer");
             fiDOMDocumentSerializer_new = clazz.getConstructor();
             fiDOMDocumentSerializer_serialize = clazz.getMethod("serialize", 
                 new Class[] { org.w3c.dom.Node.class });
             fiDOMDocumentSerializer_setOutputStream = clazz.getMethod("setOutputStream",
                 new Class[] { java.io.OutputStream.class });
             
-            fiFastInfosetSource = clazz = Class.forName("org.jvnet.fastinfoset.FastInfosetSource",
-                    true, cl);
+            fiFastInfosetSource = clazz = Class.forName("org.jvnet.fastinfoset.FastInfosetSource");
             fiFastInfosetSource_new = clazz.getConstructor(
                 new Class[] { java.io.InputStream.class });
             fiFastInfosetSource_getInputStream = clazz.getMethod("getInputStream");          
             fiFastInfosetSource_setInputStream = clazz.getMethod("setInputStream", 
                 new Class[] { java.io.InputStream.class });          
             
-            fiFastInfosetResult = clazz = Class.forName("org.jvnet.fastinfoset.FastInfosetResult",
-                    true, cl);
+            fiFastInfosetResult = clazz = Class.forName("org.jvnet.fastinfoset.FastInfosetResult");
             fiFastInfosetResult_new = clazz.getConstructor(
                 new Class[] { java.io.OutputStream.class });
             fiFastInfosetResult_getOutputStream = clazz.getMethod("getOutputStream");           
