700c7dd04df13909fb2b982db48aed025ed80903
Omar Fung <omar.fung@sun.com>
2006-06-22 21:59:51 +0000

    Update for Copyright 2006 Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-ri/samples/mime/src/mime/client/MimeApp.java
+++ jaxws-ri/samples/mime/src/mime/client/MimeApp.java
@@ -1,5 +1,27 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+
 /**
- * $Id: MimeApp.java,v 1.5 2006-03-22 00:55:11 vivekp Exp $
+ * $Id: MimeApp.java,v 1.6 2006-06-22 21:59:38 ofung Exp $
  */
 
 /*
--- jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
+++ jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
@@ -1,5 +1,27 @@
+/*
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at
+ * https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * See the License for the specific language governing
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at https://glassfish.dev.java.net/public/CDDLv1.0.html.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems Inc. All Rights Reserved
+ */
+
 /**
- * $Id: HelloImpl.java,v 1.2 2005-09-10 19:49:12 kohsuke Exp $
+ * $Id: HelloImpl.java,v 1.3 2006-06-22 21:59:38 ofung Exp $
  */
 package mime.server;
 
