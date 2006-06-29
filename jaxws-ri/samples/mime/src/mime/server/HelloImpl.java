07e2b4cee21a6b67dfca86d1b154062f48e619e6
Omar Fung <omar.fung@sun.com>
2006-06-29 22:30:16 +0000

    Removed double copyright text for 2006 Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
+++ jaxws-ri/samples/mime/src/mime/server/HelloImpl.java
@@ -21,29 +21,10 @@
  */
 
 /**
- * $Id: HelloImpl.java,v 1.3 2006-06-22 21:59:38 ofung Exp $
+ * $Id: HelloImpl.java,v 1.4 2006-06-29 22:30:04 ofung Exp $
  */
 package mime.server;
 
-/*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- * 
- * You can obtain a copy of the license at
- * https://jwsdp.dev.java.net/CDDLv1.0.html
- * See the License for the specific language governing
- * permissions and limitations under the License.
- * 
- * When distributing Covered Code, include this CDDL
- * HEADER in each file and include the License file at
- * https://jwsdp.dev.java.net/CDDLv1.0.html  If applicable,
- * add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your
- * own identifying information: Portions Copyright [yyyy]
- * [name of copyright owner]
- */
 import javax.jws.WebService;
 import javax.xml.ws.Holder;
 import javax.xml.transform.Source;
