b236f7defa504be6b17e438de361d02ca55ba1e9
Vivek Pandey <vivek.pandey@sun.com>
2006-10-02 22:06:30 +0000

    Fixed earlier CVS import problem. brought testcases one level up.

---
+++ jaxws-unit/testcases/fromjava/type_substitution/abstractclass/server/Toyota.java
@@ -0,0 +1,33 @@
+package fromjava.type_substitution.abstractclass.server;
+
+public class Toyota extends Car {
+    private String color;
+    private final String make="Toyota";
+
+    public Toyota() {
+        setMake("Toyota");
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public Toyota(String model, String year, String color) {
+        setModel(model);
+        setYear(year);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString(){
+        return getMake()+":"+getModel()+":"+getYear()+":"+color;
+    }
+
+}
