ac5d2713d37022caf8b8d1cde5616c859ae38faf
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-11 19:02:30 +0000

    adding the first stateful web service test.

---
+++ jaxws-unit/testcases/fromjava/stateful/bank/Account.java
@@ -0,0 +1,53 @@
+package fromjava.stateful.bank;
+
+import com.sun.xml.ws.developer.Stateful;
+import com.sun.xml.ws.developer.StatefulWebServiceManager;
+
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.Feature;
+import javax.xml.ws.WebServiceException;
+import javax.xml.ws.soap.AddressingFeature;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@Stateful
+@WebService
+@BindingType(features=@Feature(AddressingFeature.ID))
+public class Account {
+    private final int id;
+    private int amount;
+
+    /*package*/ Account(int id) {
+        this.id = id;
+    }
+
+    public void deposit(int delta) {
+        amount += delta;
+    }
+
+    public int getBalance() {
+        return amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void destroy() {
+        manager.unexport(this);
+    }
+
+    /*package*/ static StatefulWebServiceManager<Account> manager;
+
+    // to be injected by JAX-WS RI
+    public static void setStatefulManager(StatefulWebServiceManager<Account> m) {
+        manager = m;
+        manager.setFallbackInstance(new Account(-1) {
+            public int getBalance() {
+                throw new WebServiceException("no such account");
+            }
+        });
+    }
+}
---
+++ jaxws-unit/testcases/fromjava/stateful/bank/BankService.java
@@ -0,0 +1,15 @@
+package fromjava.stateful.bank;
+
+import javax.jws.WebService;
+import javax.xml.ws.wsaddressing.W3CEndpointReference;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@WebService
+public class BankService {
+    public W3CEndpointReference getAccount(int id) {
+        Account a = new Account(id);
+        return Account.manager.export(a);
+    }
+}
