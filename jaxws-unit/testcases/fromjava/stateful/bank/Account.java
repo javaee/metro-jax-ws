0a5ef436ec32bfffb35882aa067523b34650187f
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-12 00:32:06 +0000

    expanding test coverage to test unexport and timeout.

--- jaxws-unit/testcases/fromjava/stateful/bank/Account.java
+++ jaxws-unit/testcases/fromjava/stateful/bank/Account.java
@@ -2,6 +2,7 @@ package fromjava.stateful.bank;
 
 import com.sun.xml.ws.developer.ServerFeatures;
 import com.sun.xml.ws.developer.StatefulWebServiceManager;
+import com.sun.xml.ws.developer.StatefulWebServiceManager.Callback;
 
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
@@ -18,7 +19,7 @@ public class Account {
     private final int id;
     private int amount;
 
-    /*package*/ Account(int id) {
+    public Account(int id) {
         this.id = id;
     }
 
@@ -43,6 +44,14 @@ public class Account {
     // to be injected by JAX-WS RI
     public static void setStatefulManager(StatefulWebServiceManager<Account> m) {
         manager = m;
+        manager.setTimeout(1000,new Callback<Account>() {
+            public void onTimeout(Account o, StatefulWebServiceManager manager) {
+                if(o.id<10000)
+                    manager.touch(o);
+                else
+                    manager.unexport(o);
+            }
+        });
         manager.setFallbackInstance(new Account(-1) {
             public int getBalance() {
                 throw new WebServiceException("no such account");
