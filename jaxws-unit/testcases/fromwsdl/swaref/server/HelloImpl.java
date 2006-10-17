e4598da41ccbbf2c17d3bf3d344171875cb4a28e
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-10-17 18:24:39 +0000

    simplified.

--- jaxws-unit/testcases/fromwsdl/swaref/server/HelloImpl.java
+++ jaxws-unit/testcases/fromwsdl/swaref/server/HelloImpl.java
@@ -4,26 +4,9 @@ import javax.jws.WebService;
 
 @WebService(endpointInterface = "fromwsdl.swaref.server.Hello")
 public class HelloImpl {
-
-
     public ClaimFormTypeResponse claimForm(ClaimFormTypeRequest data){
         ClaimFormTypeResponse resp = new ClaimFormTypeResponse();
         resp.setResponse(data.getRequest());
         return resp;
     }
-
-    private final String sampleXML = "?xml version=\"1.0\" encoding=\"UTF-8\" ?> \n" +
-            "<NMEAstd>\n" +
-            "<DevIdSentenceId>$GPRMC</DevIdSentenceId>\n" +
-            "<Time>212949</Time>\n" +
-            "<Navigation>A</Navigation>\n" +
-            "<NorthOrSouth>4915.61N</NorthOrSouth>\n" +
-            "<WestOrEast>12310.55W</WestOrEast>\n" +
-            "<SpeedOnGround>000.0</SpeedOnGround>\n" +
-            "<Course>360.0</Course>\n" +
-            "<Date>030904</Date>\n" +
-            "<MagneticVariation>020.3</MagneticVariation>\n" +
-            "<MagneticPoleEastOrWest>E</MagneticPoleEastOrWest>\n" +
-            "<ChecksumInHex>*6B</ChecksumInHex>\n" +
-            "</NMEAstd>";
 }
