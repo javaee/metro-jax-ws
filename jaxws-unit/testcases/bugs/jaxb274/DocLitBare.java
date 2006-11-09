a523eae561b15f1620cbd7944b66e24e25f1ad22
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2006-11-09 01:33:53 +0000

    tried to reproduce the regression but failed.

---
+++ jaxws-unit/testcases/bugs/jaxb274/DocLitBare.java
@@ -0,0 +1,19 @@
+package bugs.jaxb274;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import static javax.jws.soap.SOAPBinding.ParameterStyle.BARE;
+import static javax.jws.soap.SOAPBinding.Style.DOCUMENT;
+import static javax.jws.soap.SOAPBinding.Use.LITERAL;
+
+@WebService
+@SOAPBinding(style=DOCUMENT, use=LITERAL ,parameterStyle=BARE)
+public class DocLitBare {
+    public Person[] echo(Person[] people){
+        System.out.println("Received array. length="+people.length);
+        for (Person p : people) {
+            System.out.println(p);
+        }
+        return people;
+    }
+}
