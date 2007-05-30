75ca1ff1722c5ae804fefb9441ea177bbbd33f56
Omar Fung <omar.fung@sun.com>
2007-05-30 01:21:40 +0000

    Fixed residual block from previous copyright text Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-ri/rt/src/com/sun/xml/ws/server/sei/EndpointMethodDispatcher.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/server/sei/EndpointMethodDispatcher.java
@@ -1,23 +1,4 @@
 /*
- The contents of this file are subject to the terms
- of the Common Development and Distribution License
- (the "License").  You may not use this file except
- in compliance with the License.
- 
- You can obtain a copy of the license at
- https://jwsdp.dev.java.net/CDDLv1.0.html
- See the License for the specific language governing
- permissions and limitations under the License.
- 
- When distributing Covered Code, include this CDDL
- HEADER in each file and include the License file at
- https://jwsdp.dev.java.net/CDDLv1.0.html  If applicable,
- add the following below this CDDL HEADER, with the
- fields enclosed by brackets "[]" replaced with your
- own identifying information: Portions Copyright [yyyy]
- [name of copyright owner]
-*/
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
@@ -53,7 +34,7 @@
  * holder.
  */
 /*
- * $Id: EndpointMethodDispatcher.java,v 1.4 2007-05-30 00:59:47 ofung Exp $
+ * $Id: EndpointMethodDispatcher.java,v 1.5 2007-05-30 01:21:36 ofung Exp $
  */
 
 package com.sun.xml.ws.server.sei;
