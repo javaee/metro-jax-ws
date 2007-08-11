b94c17532bcca239a90630d1355bf65827116a77
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2007-08-11 01:01:58 +0000

    Adding WebServiceContext test.

---
+++ jaxws-unit/testcases/provider/wsdl_hello_lit_context/server/Hello_PortType_Impl.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package provider.wsdl_hello_lit_context.server;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.WebServiceProvider;
+import javax.xml.ws.WebServiceException;
+import javax.jws.HandlerChain;
+
+/**
+ * @author Jitendra Kotamraju
+ */
+@WebServiceProvider(serviceName="Hello", portName="HelloPort",
+    targetNamespace="urn:test",
+    wsdlLocation="WEB-INF/wsdl/hello_literal_overridden.wsdl")
+public class Hello_PortType_Impl extends ProviderImpl {
+    @Resource(type=Object.class)
+    protected WebServiceContext wsContext;
+
+    private boolean injectionDone;
+
+    public WebServiceContext getContext() {
+        return wsContext;
+    }
+
+    @PostConstruct
+    public void over() {
+        System.out.println("PostConstruct Complete");
+        injectionDone = true;
+        boolean illegal = false;
+        try {
+        	wsContext.getMessageContext();
+        } catch(IllegalStateException ie) {
+			// No op. Expected to get this exception
+            illegal = true;
+        }
+        if (!illegal) {
+            throw new WebServiceException("IllegalStateException is not called");
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("PreDestroy is called");
+        boolean illegal = false;
+        try {
+        	wsContext.getMessageContext();
+        } catch(IllegalStateException ie) {
+			// No op. Expected to get this exception
+            illegal = true;
+        }
+        if (!illegal) {
+            throw new WebServiceException("IllegalStateException is not called");
+        }
+    }
+
+    public boolean isInjectionDone() {
+        return injectionDone;
+    }
+}
