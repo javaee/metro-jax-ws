51dbca831e7c214cb8f79919c40caafa1222876f
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2007-09-01 01:58:58 +0000

    handler fromwsdl test with rpclit wsdl

---
+++ jaxws-unit/testcases/fromwsdl/handler_simple_rpclit/server/HelloService12_Impl.java
@@ -0,0 +1,13 @@
+package fromwsdl.handler_simple_rpclit.server;
+
+/**
+ */
+@javax.jws.WebService(endpointInterface="fromwsdl.handler_simple_rpclit.server.Hello12")
+public class HelloService12_Impl implements Hello12 {
+
+    public int hello12(int x) {
+        System.out.println("Hello12Service_Impl received: " + x);
+        return x;
+    }
+
+}
---
+++ jaxws-unit/testcases/fromwsdl/handler_simple_rpclit/server/HelloService_Impl.java
@@ -0,0 +1,13 @@
+package fromwsdl.handler_simple_rpclit.server;
+
+/**
+ */
+@javax.jws.WebService(endpointInterface="fromwsdl.handler_simple_rpclit.server.Hello")
+public class HelloService_Impl implements Hello {
+
+    public int hello(int x) {
+        System.out.println("HelloService_Impl received: " + x);
+        return x;
+    }
+
+}
