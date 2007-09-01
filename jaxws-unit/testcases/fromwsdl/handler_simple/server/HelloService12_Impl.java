7f1502db47ce3db7e40d9a0d2449e3c1236cb3eb
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2007-09-01 02:38:49 +0000

    when soap 1.2 is used @BindingType should be used so that without specifying it in sun-jaxws.xml , it works

--- jaxws-unit/testcases/fromwsdl/handler_simple/server/HelloService12_Impl.java
+++ jaxws-unit/testcases/fromwsdl/handler_simple/server/HelloService12_Impl.java
@@ -1,9 +1,12 @@
 package fromwsdl.handler_simple.server;
 
 import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.SOAPBinding;
 
 /**
  */
+@BindingType(SOAPBinding.SOAP12HTTP_BINDING)
 @WebService(portName="HelloPort12", endpointInterface="fromwsdl.handler_simple.server.Hello12",portName="HelloPort12")
 public class HelloService12_Impl implements Hello12 {
 
--- jaxws-unit/testcases/fromwsdl/handler_simple_rpclit/server/HelloService12_Impl.java
+++ jaxws-unit/testcases/fromwsdl/handler_simple_rpclit/server/HelloService12_Impl.java
@@ -1,8 +1,13 @@
 package fromwsdl.handler_simple_rpclit.server;
 
+import javax.xml.ws.BindingType;
+import javax.xml.ws.soap.SOAPBinding;
+
 /**
  */
 @javax.jws.WebService(portName="HelloPort12", endpointInterface="fromwsdl.handler_simple_rpclit.server.Hello12")
+@BindingType(SOAPBinding.SOAP12HTTP_BINDING)
+
 public class HelloService12_Impl implements Hello12 {
 
     public int hello12(int x) {
