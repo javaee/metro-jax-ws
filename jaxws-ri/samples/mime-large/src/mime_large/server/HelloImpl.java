5b8236c7c09f4e6854e0ee242dcbbccd9e69037e
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2007-09-07 21:32:52 +0000

    Adding swaref sample to send large attachments

---
+++ jaxws-ri/samples/mime-large/src/mime_large/server/HelloImpl.java
@@ -0,0 +1,116 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package mime_large.server;
+
+import javax.jws.WebService;
+import javax.xml.ws.Holder;
+import javax.xml.ws.WebServiceException;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import java.io.*;
+
+/**
+ * @author Jitendra Kotamraju
+ */
+
+@WebService (endpointInterface = "mime_large.server.Hello")
+public class HelloImpl {
+
+    public ClaimFormTypeResponse claimForm(ClaimFormTypeRequest data) {
+        try {
+    	    validateDataHandler(data.getTotal(), data.getRequest());
+        } catch(IOException ioe) {
+            throw new WebServiceException(ioe);
+        }
+
+        ClaimFormTypeResponse resp = new ClaimFormTypeResponse();
+        resp.setTotal(data.getTotal()+1);
+        resp.setResponse(getDataHandler(data.getTotal()+1));
+        return resp;
+    }
+
+    private DataHandler getDataHandler(final int total)  {
+        return new DataHandler(new DataSource() {
+            public InputStream getInputStream() throws IOException {
+                return new InputStream() {
+                    int i;
+
+                    @Override
+                    public int read() throws IOException {
+                        return i<total ? 'A'+(i++%26) : -1;
+                    }
+                };
+            }
+
+            public OutputStream getOutputStream() throws IOException {
+                return null;
+            }
+
+            public String getContentType() {
+                return "application/octet-stream";
+            }
+
+            public String getName() {
+                return "";
+            }
+        });
+    }
+
+    private void validateDataHandler(int expTotal, DataHandler dh) throws IOException {
+        // readOnce() doesn't store attachment on the disk in some cases
+        // for e.g when only one attachment is in the message
+        InputStream in = ((com.sun.xml.ws.developer.StreamingDataHandler)dh).readOnce();
+        byte[] buf = new byte[8192];
+        int total = 0;
+        int len;
+        while((len=in.read(buf, 0, buf.length)) != -1) {
+            for(int i=0; i < len; i++) {
+                if ((byte)('A'+(total+i)%26) != buf[i]) {
+                    throw new WebServiceException("DataHandler data is different");
+                }
+            }
+            total += len;
+            if (total%(8192*250) == 0) {
+            	System.out.println("Total so far="+total);
+            }
+        }
+        in.close();
+        if (total != expTotal) {
+           throw new WebServiceException("DataHandler data size is different");
+        }
+    }
+}
