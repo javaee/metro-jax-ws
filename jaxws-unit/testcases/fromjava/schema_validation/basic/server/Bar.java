63fdff0eb2daa26e1ddee06ddd84107d02e7c39f
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2007-10-09 22:07:42 +0000

    fromjava schema validation test

---
+++ jaxws-unit/testcases/fromjava/schema_validation/basic/server/Bar.java
@@ -0,0 +1,20 @@
+
+package fromjava.schema_validation.basic.server;
+
+public class Bar {
+    private int age; 
+   
+    public Bar() {}
+
+    public Bar(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
