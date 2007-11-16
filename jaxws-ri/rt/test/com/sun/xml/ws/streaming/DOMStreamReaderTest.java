dab1af756e20780f48c10e6630dda6fb761b8cd6
Kohsuke Kawaguchi <kohsuke.kawaguchi@sun.com>
2007-11-16 17:53:14 +0000

    added a regression test for #727.

--- jaxws-ri/rt/test/com/sun/xml/ws/streaming/DOMStreamReaderTest.java
+++ jaxws-ri/rt/test/com/sun/xml/ws/streaming/DOMStreamReaderTest.java
@@ -2,9 +2,13 @@ package com.sun.xml.ws.streaming;
 
 import junit.framework.TestCase;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamConstants;
+import java.io.IOException;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -14,10 +18,7 @@ public class DOMStreamReaderTest extends TestCase {
      * https://jax-ws.dev.java.net/issues/show_bug.cgi?id=464
      */
     public void test464() throws Exception {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setNamespaceAware(true);
-        Document dom = dbf.newDocumentBuilder().parse(getClass().getResourceAsStream("issue464.xml"));
-        XMLStreamReader r = new DOMStreamReader(dom);
+        XMLStreamReader r = load("issue464.xml");
 
         r.nextTag();
         assertEquals(1,r.getNamespaceCount());
@@ -29,5 +30,31 @@ public class DOMStreamReaderTest extends TestCase {
         assertEquals(0,r.getNamespaceCount());
         r.nextTag();
         assertEquals(1,r.getNamespaceCount());
+    }
+
+    /**
+     * https://wsit.dev.java.net/issues/show_bug.cgi?id=727
+     */
+    public void test727() throws Exception {
+        XMLStreamReader r = load("wsit-727.xml");
+
+        while(r.hasNext()) {
+            switch(r.next()) {
+            case XMLStreamConstants.START_ELEMENT:
+            case XMLStreamConstants.END_ELEMENT:
+                // call some of the stream reader methods to make it do some work
+                assertEquals(0,r.getNamespaceCount());
+                r.getLocalName();
+                assertEquals("",r.getNamespaceURI());
+                break;
+            }
+        }
+    }
+
+    private XMLStreamReader load(String resourceName) throws SAXException, IOException, ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        Document dom = dbf.newDocumentBuilder().parse(getClass().getResourceAsStream(resourceName));
+        return new DOMStreamReader(dom);
     }
 }
