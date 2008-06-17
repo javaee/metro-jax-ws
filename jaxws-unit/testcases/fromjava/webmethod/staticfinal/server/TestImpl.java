b13cde2f9944d150cc6f5683ae31872c358145a7
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2008-06-17 21:56:57 +0000

    Adding a test for static and final methods(which shouldn't qualify for web methods)

---
+++ jaxws-unit/testcases/fromjava/webmethod/staticfinal/server/TestImpl.java
@@ -0,0 +1,46 @@
+package fromjava.webmethod.declclass.server;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+
+/**
+ * non static or non final methods are web methods
+ *
+ * @author Jitendra Kotamraju
+ */
+@WebService
+public class TestImpl {
+
+    @WebMethod
+    public String method1(String str) {
+        return str;
+    }
+
+    // Not a web method
+    @WebMethod(exclude=true)
+    public static String method2(String str) {
+        return str;
+    }
+
+    // Not a web method
+    @WebMethod(exclude=true)
+    public final String method3(String str) {
+        return str;
+    }
+
+    // Not a web method
+    public static String method4(String str) {
+        return str;
+    }
+
+    // Not a web method
+    public final String method5(String str) {
+        return str;
+    }
+
+    // This is also a WebMethod since declaring class
+    // has WebService
+    public String method6(String str) {
+        return str;
+    }
+}
