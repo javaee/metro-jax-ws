106f17495478b3ab40d28ec86f6efac8cb546112
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2008-07-01 22:45:36 +0000

    removing unnecessary imports Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-unit/testcases/server/http_session_scope/server/HelloServiceImpl.java
+++ jaxws-unit/testcases/server/http_session_scope/server/HelloServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: HelloServiceImpl.java,v 1.1 2008-06-16 23:15:01 jitu Exp $
+ * $Id: HelloServiceImpl.java,v 1.2 2008-07-01 22:45:36 jitu Exp $
  *
  * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -13,9 +13,6 @@ import javax.annotation.Resource;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequest;
 
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.WebServiceException;
