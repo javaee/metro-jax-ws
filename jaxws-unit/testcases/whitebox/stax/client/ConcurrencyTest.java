7c6a57b4fd9738c0aad91ec5f43139e56874f2c8
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2008-07-22 23:28:03 +0000

    Adding a concurrency test for stax parser usage. Issue number: Obtained from: Submitted by: Reviewed by:

---
+++ jaxws-unit/testcases/whitebox/stax/client/ConcurrencyTest.java
@@ -0,0 +1,99 @@
+package whitebox.stax.client;
+
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.*;
+
+import junit.framework.TestCase;
+
+import com.sun.xml.ws.api.streaming.XMLStreamReaderFactory;
+import com.sun.xml.ws.api.streaming.XMLStreamWriterFactory;
+
+/**
+ * @author Jitendra Kotamraju
+ */
+public class ConcurrencyTest extends TestCase {
+
+    private static final int NO_THREADS = 10;
+    private static final int PER_THREAD = 5000;
+
+    public void test() throws Exception {
+        Thread[] threads = new Thread[NO_THREADS];
+        MyRunnable[] run = new MyRunnable[NO_THREADS];
+        for (int i = 0; i < NO_THREADS; i++) {
+            run[i] = new MyRunnable(i);
+            threads[i] = new Thread(run[i]);
+        }
+        for (int i = 0; i < NO_THREADS; i++) {
+            threads[i].start();
+        }
+        for (int i = 0; i < NO_THREADS; i++) {
+            threads[i].join();
+        }
+        for (int i = 0; i < NO_THREADS; i++) {
+            Exception e = run[i].getException();
+            if (e != null) {
+                e.printStackTrace();
+                fail("Failed with exception."+e);
+            }
+        }
+    }
+
+    private static class MyRunnable implements Runnable {
+        final int no;
+        volatile Exception e;
+
+        MyRunnable(int no) {
+            this.no = no;
+        }
+
+        public void run() {
+            for(int req=0; req < PER_THREAD && e == null; req++) {
+                try {
+                    File file = File.createTempFile("jaxws-unit-stax", "xml");
+
+                    FileOutputStream fos = new FileOutputStream(file);
+                    XMLStreamWriter w = getWriter(fos);
+                    //System.out.println("Writer="+w+" Thread="+Thread.currentThread());
+                    w.writeStartDocument();
+                    w.writeStartElement("hello");
+                    for (int j = 0; j < 50; j++) {
+                        w.writeStartElement("a" + j);
+                        w.writeEndElement();
+                    }
+                    w.writeEndElement();
+                    w.writeEndDocument();
+                    w.close();
+                    fos.close();
+
+                    FileInputStream fis = new FileInputStream(file);
+                    XMLStreamReader r = getReader(fis);
+                    while (r.hasNext()) {
+                        r.next();
+                    }
+                    r.close();
+                    fis.close();
+
+                    file.delete();
+                } catch (Exception e) {
+                    this.e = e;
+                }
+            }
+        }
+
+        public Exception getException() {
+            return e;
+        }
+    }
+
+    private static XMLStreamReader getReader(InputStream is)
+            throws Exception {
+        return XMLStreamReaderFactory.create(null, is, true);
+    }
+
+    private static XMLStreamWriter getWriter(OutputStream os)
+            throws Exception {
+        return XMLStreamWriterFactory.create(os);
+    }
+
+}
