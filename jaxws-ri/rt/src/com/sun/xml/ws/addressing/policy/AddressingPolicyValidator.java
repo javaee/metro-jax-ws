6f499b33228e73980e22c848fcc158ac783cb581
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2009-01-13 01:13:09 +0000

    Validate the Addressing assertion and its nested assertions.

--- jaxws-ri/rt/src/com/sun/xml/ws/addressing/policy/AddressingPolicyValidator.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/addressing/policy/AddressingPolicyValidator.java
@@ -38,22 +38,30 @@ package com.sun.xml.ws.addressing.policy;
 
 import com.sun.xml.ws.api.addressing.AddressingVersion;
 import com.sun.xml.ws.policy.PolicyAssertion;
+import com.sun.xml.ws.policy.NestedPolicy;
+import com.sun.xml.ws.policy.privateutil.PolicyLogger;
 import com.sun.xml.ws.policy.spi.PolicyAssertionValidator;
 import com.sun.xml.ws.policy.spi.PolicyAssertionValidator.Fitness;
+import com.sun.xml.ws.addressing.W3CAddressingMetadataConstants;
+
 import java.util.ArrayList;
 import javax.xml.namespace.QName;
+import javax.xml.ws.WebServiceException;
 
 /**
- *
+ * This class validates the Addressing assertions.
+ * If the assertion is wsam:Addressing, it makes sure that only valid assertions are nested.
+ *  
  * @author japod
+ * @author Rama Pulavarthi
  */
-public class AddressingPolicyValidator implements PolicyAssertionValidator{
-    
+public class AddressingPolicyValidator implements PolicyAssertionValidator {
+
     private static final ArrayList<QName> supportedAssertions = new ArrayList<QName>();
-    
+
     static {
-        supportedAssertions.add(new QName(AddressingVersion.MEMBER.policyNsUri,"UsingAddressing"));
-        supportedAssertions.add(new QName(AddressingVersion.W3C.policyNsUri,"UsingAddressing"));
+        supportedAssertions.add(new QName(AddressingVersion.MEMBER.policyNsUri, "UsingAddressing"));
+        supportedAssertions.add(new QName(AddressingVersion.W3C.policyNsUri, "UsingAddressing"));
         supportedAssertions.add(new QName("http://www.w3.org/2007/05/addressing/metadata", "Addressing"));
         supportedAssertions.add(new QName("http://www.w3.org/2007/05/addressing/metadata", "AnonymousResponses"));
         supportedAssertions.add(new QName("http://www.w3.org/2007/05/addressing/metadata", "NonAnonymousResponses"));
@@ -70,10 +78,36 @@ public class AddressingPolicyValidator implements PolicyAssertionValidator{
     }
 
     public Fitness validateServerSide(PolicyAssertion assertion) {
-        return supportedAssertions.contains(assertion.getName()) ? Fitness.SUPPORTED : Fitness.UNKNOWN;
+        if (supportedAssertions.contains(assertion.getName()))
+            return Fitness.UNKNOWN;
+
+        //Make sure wsam:Addressing contains only one of the allowed nested assertions.
+        if (assertion.getName().equals(W3CAddressingMetadataConstants.WSAM_ADDRESSING_ASSSSERTION)) {
+            NestedPolicy nestedPolicy = assertion.getNestedPolicy();
+            boolean requiresAnonymousResponses = false;
+            boolean requiresNonAnonymousResponses = false;
+            for (PolicyAssertion nestedAsser : nestedPolicy.getAssertionSet()) {
+                if (nestedAsser.getName().equals(W3CAddressingMetadataConstants.WSAM_ANONYMOUS_NESTED_ASSSSERTION)) {
+                    requiresAnonymousResponses = true;
+                } else if (nestedAsser.getName().equals(W3CAddressingMetadataConstants.WSAM_NONANONYMOUS_NESTED_ASSSSERTION)) {
+                    requiresNonAnonymousResponses = true;
+                } else {
+                    return Fitness.UNSUPPORTED;
+                }
+            }
+
+            if (requiresAnonymousResponses && requiresNonAnonymousResponses) {
+                LOGGER.warning("Only one among AnonymousResponses and NonAnonymousResponses can be nested in an Addressing assertion");
+                return Fitness.INVALID;
+            }
+        }
+
+        return Fitness.SUPPORTED;
     }
 
     public String[] declareSupportedDomains() {
-        return new String[] {AddressingVersion.MEMBER.policyNsUri, AddressingVersion.W3C.policyNsUri, "http://www.w3.org/2007/05/addressing/metadata"};
+        return new String[]{AddressingVersion.MEMBER.policyNsUri, AddressingVersion.W3C.policyNsUri, "http://www.w3.org/2007/05/addressing/metadata"};
     }
+
+    private static final PolicyLogger LOGGER = PolicyLogger.getLogger(AddressingPolicyValidator.class);
 }
