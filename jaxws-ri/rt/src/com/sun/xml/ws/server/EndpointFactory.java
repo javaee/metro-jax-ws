f8fc9797ced6e33d2555ee7c0fdc3d69cd6cf25f
Fabian Ritzmann <fabian.ritzmann@oracle.com>
2009-02-03 09:40:03 +0000

    Initialize policy map for providers with WSDL.

--- jaxws-ri/rt/src/com/sun/xml/ws/server/EndpointFactory.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/server/EndpointFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -180,6 +180,7 @@ public class EndpointFactory {
         if (implType.getAnnotation(WebServiceProvider.class)!=null) {
             //Provider case: Enable Addressing from WSDL only if it has RespectBindingFeature enabled
             if(wsdlPort != null) {
+                 policyMap = wsdlPort.getOwner().getParent().getPolicyMap();
                  //Merge features from WSDL and other policy configuration
                  features.mergeFeatures(wsdlPort,true,true);
             } else {
