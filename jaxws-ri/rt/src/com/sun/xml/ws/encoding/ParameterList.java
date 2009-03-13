34898dfee7c6ab3a2d22b0a4d5487f034625e236
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2009-03-13 03:39:32 +0000

    Fix the copyright header to CDDL+GPL v2 on all files.

--- jaxws-ri/rt/src/com/sun/xml/ws/encoding/ParameterList.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/encoding/ParameterList.java
@@ -1,30 +1,3 @@
-package com.sun.xml.ws.encoding;
-
-
-
-/*
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- *
- * You can obtain a copy of the license at
- * https://jwsdp.dev.java.net/CDDLv1.0.html
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * HEADER in each file and include the License file at
- * https://jwsdp.dev.java.net/CDDLv1.0.html  If applicable,
- * add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your
- * own identifying information: Portions Copyright [yyyy]
- * [name of copyright owner]
- */
-/*
- * @(#)ParameterList.java     1.10 03/02/12
- */
-
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
@@ -60,6 +33,9 @@ package com.sun.xml.ws.encoding;
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+
+package com.sun.xml.ws.encoding;
+
 import javax.xml.ws.WebServiceException;
 import java.util.HashMap;
 import java.util.Iterator;
