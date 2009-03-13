a9afd5a797ef7458b21d16483faa9ac12cf97fdb
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2009-03-13 03:55:14 +0000

    Fix the copyright header to CDDL+GPL v2 on all files.

--- jaxws-ri/tools/scripts/in-cvs.sh
+++ jaxws-ri/tools/scripts/in-cvs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -x
-
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
-# 
+#
 # Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
-# 
+#
 # The contents of this file are subject to the terms of either the GNU
 # General Public License Version 2 only ("GPL") or the Common Development
 # and Distribution License("CDDL") (collectively, the "License").  You
@@ -12,7 +11,7 @@
 # a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
 # or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
 # language governing permissions and limitations under the License.
-# 
+#
 # When distributing the software, include this License Header Notice in each
 # file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
 # Sun designates this particular file as subject to the "Classpath" exception
@@ -21,9 +20,9 @@
 # Header, with the fields enclosed by brackets [] replaced by your own
 # identifying information: "Portions Copyrighted [year]
 # [name of copyright owner]"
-# 
+#
 # Contributor(s):
-# 
+#
 # If you wish your version of this file to be governed by only the CDDL or
 # only the GPL Version 2, indicate your decision by adding "[Contributor]
 # elects to include this software in this distribution under the [CDDL or GPL
@@ -35,6 +34,7 @@
 # only if the new code is made subject to such option by the copyright
 # holder.
 #
+
 
 # checks if the argument is under the control of CVS.
 # if it's in, return 0, otherwise 1
