d555b84829da084a2ec453aedfa272f16fb220ca
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2009-04-10 19:00:20 +0000

    Now it checks if the received byte[] is correct

--- jaxws-unit/testcases/fromjava/xml_java_type_adapter/server/EchoImpl.java
+++ jaxws-unit/testcases/fromjava/xml_java_type_adapter/server/EchoImpl.java
@@ -1,19 +1,10 @@
 
 package fromjava.xml_java_type_adapter.server;
 
-import javax.jws.*;
-
-import javax.xml.ws.Holder;
-import javax.xml.ws.*;
-import javax.xml.soap.*;
-import javax.xml.ws.soap.SOAPFaultException;
-import javax.xml.namespace.QName;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.jws.WebService;
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.rmi.RemoteException;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.ws.WebServiceException;
 
 @WebService(name="Echo", serviceName="echoService", targetNamespace="http://echo.org/")
 public class EchoImpl {
@@ -22,6 +13,14 @@ public class EchoImpl {
     public byte[] echo(String str,
                        @XmlJavaTypeAdapter(HexBinaryAdapter.class)
                        byte[] bin) {
+        if (!str.equals("binary")) {
+            throw new WebServiceException("Expecting=binary, but got="+str);
+        }
+        if (bin == null || bin.length != 4 || bin[0] != (byte)0xCA
+                || bin[1] != (byte)0xFE || bin[2] != (byte)0xBA
+                || bin[3] != (byte)0xBE) {
+            throw new WebServiceException("Didn't receive CAFEBABE.");
+        }
         return bin;
     }
     
