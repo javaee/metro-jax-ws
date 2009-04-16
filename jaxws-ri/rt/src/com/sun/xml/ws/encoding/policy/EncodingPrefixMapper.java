3ea371046155ecc20a8dd614cac17e80e954ac4c
Fabian Ritzmann <fabian.ritzmann@oracle.com>
2009-04-16 09:15:52 +0000

    Complement encoding namespace prefixes.

--- jaxws-ri/rt/src/com/sun/xml/ws/encoding/policy/EncodingPrefixMapper.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/encoding/policy/EncodingPrefixMapper.java
@@ -48,8 +48,8 @@ public class EncodingPrefixMapper implements PrefixMapper {
     private static final Map<String, String> prefixMap = new HashMap<String, String>();
 
     static {
-//        prefixMap.put(com.sun.xml.ws.encoding.policy.EncodingConstants.OPTIMIZED_MIME_NS, "");
-//        prefixMap.put(com.sun.xml.ws.encoding.policy.EncodingConstants.ENCODING_NS, "");
+        prefixMap.put(EncodingConstants.ENCODING_NS, "wspe");
+        prefixMap.put(EncodingConstants.OPTIMIZED_MIME_NS, "wsoma");
         prefixMap.put(EncodingConstants.SUN_ENCODING_CLIENT_NS, "cenc");
         prefixMap.put(EncodingConstants.SUN_FI_SERVICE_NS, "fi");
     }
