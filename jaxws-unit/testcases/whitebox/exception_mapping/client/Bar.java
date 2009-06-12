3c8c3a1a9f42d0c06c69b7be8e0c34647590cf9a
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2009-06-12 19:15:40 +0000

    JAX-WS-770: Adding testcase for dynamic exception mapping from Java to WSDL.

---
+++ jaxws-unit/testcases/whitebox/exception_mapping/client/Bar.java
@@ -0,0 +1,25 @@
+
+package whitebox.exception_mapping.client;
+
+public class Bar {
+   private int age;
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Bar() {}
+
+   public int getAge() {
+       return age;
+   }
+
+   public void setAge(int age) {
+       this.age = age;
+   }
+}
