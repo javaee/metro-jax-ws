642f907d6dd36dbbf6c2d775d78edde68f95c35a
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2009-06-19 00:23:16 +0000

    Added tests for other Endpoint API methods that use WebServiceFeature

--- jaxws-unit/testcases/server/endpoint_features/client/EndpointFeaturesTest.java
+++ jaxws-unit/testcases/server/endpoint_features/client/EndpointFeaturesTest.java
@@ -43,6 +43,7 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.ws.Endpoint;
 import javax.xml.ws.soap.AddressingFeature;
+import javax.xml.ws.soap.SOAPBinding;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -57,20 +58,49 @@ public class EndpointFeaturesTest extends TestCase {
     private static final QName ANONYMOUS =
             new QName("http://www.w3.org/2007/05/addressing/metadata", "AnonymousResponses");
 
-    public void testMetadata() throws Exception {
+    // Tests Endpoint.create(impl, features)
+    public void testCreateImplFeatures() throws Exception {
         int port = Util.getFreePort();
         String address = "http://localhost:" + port + "/hello";
         Endpoint endpoint = Endpoint.create(new FeaturesEndpoint(),
                 new AddressingFeature(true, true, AddressingFeature.Responses.ANONYMOUS));
-        endpoint.publish(address);
+        publishVerifyStop(address, endpoint);
+    }
 
-        // Check if the generated WSDL contains wsam:AnonymousResponses policy
+    // Tests Endpoint.publish(address, impl, features)
+    public void testPublish() throws Exception {
+        int port = Util.getFreePort();
+        String address = "http://localhost:" + port + "/hello";
+        Endpoint endpoint = Endpoint.publish(address, new FeaturesEndpoint(),
+                new AddressingFeature(true, true, AddressingFeature.Responses.ANONYMOUS));
+        assertTrue(endpoint.isPublished());
+        publishVerifyStop(address, endpoint);
+    }
+
+    // Tests Endpoint.create(bindingId, impl, features)
+    public void testCreateBindingImplFeatures() throws Exception {
+        int port = Util.getFreePort();
+        String address = "http://localhost:" + port + "/hello";
+        Endpoint endpoint = Endpoint.create(SOAPBinding.SOAP11HTTP_BINDING, new FeaturesEndpoint(),
+                new AddressingFeature(true, true, AddressingFeature.Responses.ANONYMOUS));
+        publishVerifyStop(address, endpoint);
+    }
+
+    // Publishes the endpoint, if it is not published.
+    // Checks if the generated WSDL contains wsam:AnonymousResponses policy
+    // Stops the endpoint
+    private void publishVerifyStop(String address, Endpoint endpoint) throws Exception {
+        if (!endpoint.isPublished()) {
+            endpoint.publish(address);
+        }
+
         URL pubUrl = new URL(address + "?wsdl");
         boolean anon = isAnonymousResponses(pubUrl);
         assertTrue(anon);
-        
+
         endpoint.stop();
     }
+
 
     // Does the generated WSDL have wsam:AnonymousResponses policy ?
     private boolean isAnonymousResponses(URL url) throws Exception {
