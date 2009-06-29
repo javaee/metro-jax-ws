594cd28064eee26a31792e54871d128ef033eb11
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2009-06-29 21:53:18 +0000

    Adding a test case for Issue: 775

--- jaxws-unit/testcases/fromjava/dynamic_775/server/EchoImpl.java
+++ jaxws-unit/testcases/fromjava/dynamic_775/server/EchoImpl.java
@@ -1,27 +1,29 @@
 
 package fromjava.dynamic_775.server;
 
-import javax.jws.*;
-
-import javax.xml.ws.Holder;
-import javax.xml.ws.*;
-import javax.xml.soap.*;
-import javax.xml.ws.soap.SOAPFaultException;
-import javax.xml.namespace.QName;
-import java.util.ArrayList;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 import java.util.List;
-import java.util.Map;
-import java.rmi.RemoteException;
 
 /**
- * Issue 775 test case
+ * Issue 775 test case. Error in generating the wrapper bean dynamically
+ * when the reflection type is GenericArrayType.
  *
  * @author Jitendra Kotamraju
  */
 @WebService(name="Echo", serviceName="echoService", targetNamespace="http://echo.org/")
 public class EchoImpl {
-    public String echo(@WebParam(name="photo") byte[] photo,
-                       @WebParam(name="groups") List<String> groups) {
+
+    // Reflection API gives "Class" for byte[] parameter
+    public String echo(@WebParam(name="photo") byte[] photo) {
         return null;
     }
+
+    // Reflection API gives "GenericArrayType" for byte[] parameter
+    // See http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=5041784
+    public String echo1(@WebParam(name="photo") byte[] photo,
+                        @WebParam(name="groups") List<String> groups) {
+        return null;
+    }
+
 }
