524e3d45dd3bc4ce1f17d336849ce1608255e263
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2009-08-21 18:30:38 +0000

    JAX-WS-795: Allow 4xx and 5xx codes sent with fault messages with SOAP 1.2 binding to go through the fault message processing.

--- jaxws-ri/rt/src/com/sun/xml/ws/transport/http/client/HttpTransportPipe.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/transport/http/client/HttpTransportPipe.java
@@ -176,7 +176,7 @@ public class HttpTransportPipe extends AbstractTubeImpl {
                 return request.createClientResponse(null);    // one way. no response given.
             }
 
-            checkStatusCode(response, con.statusCode, con.statusMessage); // throws ClientTransportException
+            checkStatusCode(response, con.statusCode, con.statusMessage, con.getHeaders()); // throws ClientTransportException
 
             String contentType = con.getContentType();
             // TODO check if returned MIME type is the same as that which was sent
@@ -193,17 +193,43 @@ public class HttpTransportPipe extends AbstractTubeImpl {
         }
     }
 
-    private void checkStatusCode(InputStream in, int statusCode, String statusMessage) throws IOException {
+    private void checkStatusCode(InputStream in, int statusCode, String statusMessage, Map<String, List<String>> respHeaders) throws IOException {
         // SOAP1.1 and SOAP1.2 differ here
         if (binding instanceof SOAPBinding) {
-            if (statusCode != HttpURLConnection.HTTP_OK && statusCode != HttpURLConnection.HTTP_INTERNAL_ERROR) {
-                if (in != null) {
-                    in.close();
+            if (binding.getSOAPVersion() == SOAPVersion.SOAP_12) {
+                //In SOAP 1.2, Fault messages can be sent with 4xx and 5xx error codes
+                if (statusCode != HttpURLConnection.HTTP_OK && !isErrorCode(statusCode)) {
+                    if (in != null) {
+                        in.close();
+                    }
+                    throw new ClientTransportException(ClientMessages.localizableHTTP_STATUS_CODE(statusCode, statusMessage));
+                } else {
+                    Headers normalizedHeaders = new Headers();
+                    normalizedHeaders.putAll(respHeaders);
+                    String contentLength = normalizedHeaders.getFirst("Content-Length");
+                    // if there is no response message, throw ClientTransportException that captures error code. 
+                    if(contentLength == null || Integer.parseInt(contentLength) == 0) {
+                        throw new ClientTransportException(ClientMessages.localizableHTTP_STATUS_CODE(statusCode, statusMessage));
+                    }
+                    //there is some response message, so let it go
                 }
-                throw new ClientTransportException(ClientMessages.localizableHTTP_STATUS_CODE(statusCode, statusMessage));
+            } else {
+                if (statusCode != HttpURLConnection.HTTP_OK && statusCode != HttpURLConnection.HTTP_INTERNAL_ERROR) {
+                    if (in != null) {
+                        in.close();
+                    }
+                    throw new ClientTransportException(ClientMessages.localizableHTTP_STATUS_CODE(statusCode, statusMessage));
+                }
             }
         }
         // Every status code is OK for XML/HTTP
+    }
+
+    private boolean isErrorCode(int code) {              
+        if(code/100 == 5/*Server-side error*/ || code/100 == 4 /*client error*/ ) {
+            return true;
+        }
+        return false;
     }
 
     private void checkStatusCodeOneway(InputStream in, int statusCode, String statusMessage) throws IOException {
