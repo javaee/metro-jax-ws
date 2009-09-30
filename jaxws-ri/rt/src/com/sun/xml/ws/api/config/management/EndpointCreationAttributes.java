65247aa6e336fb7095fe1584f85a99c484ee6956
Fabian Ritzmann <fabian.ritzmann@oracle.com>
2009-09-30 09:33:42 +0000

    Completed Javadoc.

--- jaxws-ri/rt/src/com/sun/xml/ws/api/config/management/EndpointCreationAttributes.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/config/management/EndpointCreationAttributes.java
@@ -41,6 +41,9 @@ import com.sun.xml.ws.api.server.Invoker;
 import org.xml.sax.EntityResolver;
 
 /**
+ * Store the parameters that were passed into the original WSEndpoint instance
+ * upon creation. This allows us to instantiate a new instance with the same
+ * parameters.
  *
  * @author Fabian Ritzmann
  */
@@ -51,6 +54,14 @@ public class EndpointCreationAttributes {
     private final EntityResolver entityResolver;
     private final boolean isTransportSynchronous;
 
+    /**
+     * Instantiate this data access object.
+     *
+     * @param processHandlerAnnotation The original processHandlerAnnotation setting.
+     * @param invoker The original Invoker instance.
+     * @param resolver The original EntityResolver instance.
+     * @param isTransportSynchronous The original isTransportSynchronous setting.
+     */
     public EndpointCreationAttributes(final boolean processHandlerAnnotation,
             final Invoker invoker,
             final EntityResolver resolver,
@@ -61,18 +72,38 @@ public class EndpointCreationAttributes {
         this.isTransportSynchronous = isTransportSynchronous;
     }
 
+    /**
+     * Return the original processHandlerAnnotation setting.
+     *
+     * @return The original processHandlerAnnotation setting.
+     */
     public boolean isProcessHandlerAnnotation() {
         return this.processHandlerAnnotation;
     }
 
+    /**
+     * Return the original Invoker instance.
+     *
+     * @return The original Invoker instance.
+     */
     public Invoker getInvoker() {
         return this.invoker;
     }
 
+    /**
+     * Return the original EntityResolver instance.
+     *
+     * @return The original EntityResolver instance.
+     */
     public EntityResolver getEntityResolver() {
         return this.entityResolver;
     }
 
+    /**
+     * Return the original isTransportSynchronous setting.
+     *
+     * @return The original isTransportSynchronous setting.
+     */
     public boolean isTransportSynchronous() {
         return this.isTransportSynchronous;
     }
--- jaxws-ri/rt/src/com/sun/xml/ws/api/config/management/ManagedEndpointFactory.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/api/config/management/ManagedEndpointFactory.java
@@ -39,11 +39,27 @@ package com.sun.xml.ws.api.config.management;
 import com.sun.xml.ws.api.server.WSEndpoint;
 
 /**
+ * Interface to create a new WSEndpoint wrapper. This is intended to be implemented
+ * by the configuration management to return a ManagedEndpoint that wraps the
+ * original WSEndpoint instance.
  *
  * @author Fabian Ritzmann
  */
 public interface ManagedEndpointFactory {
 
+    /**
+     * This method may return a WSEndpoint implementation that wraps the original
+     * WSEndpoint instance. This allows to interject e.g. management code. If
+     * management has not been enabled for this endpoint, it will return the original
+     * WSEndpoint instance.
+     *
+     * @param <T> The endpoint implementation type.
+     * @param endpoint The endpoint instance.
+     * @param attributes The parameters with which the original endpoint instance
+     *   was created.
+     * @return A WSEndpoint that wraps the original WSEndpoint instance or the
+     *   original WSEndpoint instance.
+     */
     public <T> WSEndpoint<T> createEndpoint(WSEndpoint<T> endpoint, EndpointCreationAttributes attributes);
 
 }
