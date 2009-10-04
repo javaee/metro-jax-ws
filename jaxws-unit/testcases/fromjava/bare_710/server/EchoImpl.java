2c673cfdd7f49d9e4ed6b45cb8d0fad00a37ae7b
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2009-10-04 04:27:31 +0000

    Expanding the test case for issue:710 Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-unit/testcases/fromjava/bare_710/server/EchoImpl.java
+++ jaxws-unit/testcases/fromjava/bare_710/server/EchoImpl.java
@@ -1,18 +1,19 @@
 
 package fromjava.bare_710.server;
 
-import javax.jws.Oneway;
-import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.xml.ws.Holder;
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.ws.*;
 
+/**
+ * fromjava doc/bare mapping
+ *
+ * @author Jitenddra Kotamraju
+ */
 @WebService(name="Echo", serviceName="EchoService", targetNamespace="http://echo.org/")
 @SOAPBinding(parameterStyle=ParameterStyle.BARE)
 public class EchoImpl {
@@ -21,10 +22,51 @@ public class EchoImpl {
                         @WebParam(mode=WebParam.Mode.OUT)
                         Holder<Integer> res) {
         if (numRequest.number1 != 10)
-            throw new WebServiceException("numRequest.number1 expected=10"+" got"+numRequest.number1);
+            throw new WebServiceException("numRequest.number1 expected=10"+" got="+numRequest.number1);
         if (numRequest.number2 != 20)
-            throw new WebServiceException("numRequest.number2 expected=10"+" got"+numRequest.number2);
+            throw new WebServiceException("numRequest.number2 expected=10"+" got="+numRequest.number2);
+        if (numRequest.guess != 25)
+            throw new WebServiceException("numRequest.guess expected=25"+" got="+numRequest.guess);
         res.value = numRequest.number1+numRequest.number2;
+    }
+
+//    > 1 IN body part. Throws an exception
+//    public void sumNumbers(NumbersRequest numRequest,
+//                        @WebParam(mode=WebParam.Mode.INOUT)
+//                        Holder<Integer> res) {
+//        if (numRequest.number1 != 10)
+//            throw new WebServiceException("numRequest.number1 expected=10"+" got"+numRequest.number1);
+//        if (numRequest.number2 != 20)
+//            throw new WebServiceException("numRequest.number2 expected=10"+" got"+numRequest.number2);
+//        if (res.value != 25)
+//            throw new WebServiceException("res.value expected=25"+" got"+res.value);
+//
+//        res.value = numRequest.number1+numRequest.number2;
+//    }
+
+    public void echoString(@WebParam(mode=WebParam.Mode.INOUT)
+                           Holder<String> str) {        
+        if (!str.value.equals("test"))
+            throw new WebServiceException("str.value expected=test"+" got="+str.value);
+    }
+
+    @WebResult(header=true)
+    public String echoHeaders(@WebParam(mode=WebParam.Mode.INOUT)
+                           Holder<String> str,
+                           @WebParam(header=true)
+                           String inHeader,
+                           @WebParam(header=true, mode=WebParam.Mode.OUT)
+                           Holder<String> outHeader,
+                           @WebParam(header=true, mode=WebParam.Mode.INOUT)
+                           Holder<String> inoutHeader) {
+        if (!str.value.equals("test"))
+            throw new WebServiceException("str.value expected=test"+" got="+str.value);
+        if (!inHeader.equals("inHeader"))
+            throw new WebServiceException("inHeader expected=inHeader"+" got="+inHeader);
+        if (!inoutHeader.value.equals("inoutHeader"))
+            throw new WebServiceException("inoutHeader expected=inoutHeader"+" got="+inoutHeader);
+        outHeader.value = "outHeader";
+        return "returnHeader";
     }
 
 }
--- jaxws-unit/testcases/fromjava/bare_710/server/NumbersRequest.java
+++ jaxws-unit/testcases/fromjava/bare_710/server/NumbersRequest.java
@@ -3,5 +3,6 @@ package fromjava.bare_710.server;
 public class NumbersRequest {
     public int number1;
     public int number2;
+    public int guess;
 }
 
