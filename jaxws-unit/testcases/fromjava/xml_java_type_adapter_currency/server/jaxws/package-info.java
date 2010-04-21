cd27f645eb802138e62c2cf115a7ae79241a3a72
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2010-04-21 23:19:29 +0000

    Adding a test case for Currency type using @XmlJavaTypeAdapter

---
+++ jaxws-unit/testcases/fromjava/xml_java_type_adapter_currency/server/jaxws/package-info.java
@@ -0,0 +1,7 @@
+@XmlJavaTypeAdapter(value= CurrencyAdapter.class,type= Currency.class)
+package fromjava.xml_java_type_adapter_currency.server.jaxws;
+
+import fromjava.xml_java_type_adapter_currency.server.CurrencyAdapter;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Currency;
