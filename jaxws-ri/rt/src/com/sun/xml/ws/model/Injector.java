306896b91effc775c1ae4b2ef5b0279467f83d31
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2010-06-23 00:16:10 +0000

    [JAX-WS-866] Calls ClassLoader#definePackage() for wrapper/exception bean packages. Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-ri/rt/src/com/sun/xml/ws/model/Injector.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/model/Injector.java
@@ -3,6 +3,7 @@ package com.sun.xml.ws.model;
 import javax.xml.ws.WebServiceException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.logging.Level;
@@ -20,11 +21,17 @@ final class Injector {
 
     private static final Method defineClass;
     private static final Method resolveClass;
+    private static final Method getPackage;
+    private static final Method definePackage;
 
     static {
         try {
             defineClass = ClassLoader.class.getDeclaredMethod("defineClass",String.class,byte[].class,Integer.TYPE,Integer.TYPE);
             resolveClass = ClassLoader.class.getDeclaredMethod("resolveClass",Class.class);
+            getPackage = ClassLoader.class.getDeclaredMethod("getPackage", String.class);
+            definePackage = ClassLoader.class.getDeclaredMethod("definePackage",
+                    String.class, String.class, String.class, String.class,
+                    String.class, String.class, String.class, URL.class);
         } catch (NoSuchMethodException e) {
             // impossible
             throw new NoSuchMethodError(e.getMessage());
@@ -35,6 +42,8 @@ final class Injector {
                 // do these setAccessible allow anyone to call these methods freely?s
                 defineClass.setAccessible(true);
                 resolveClass.setAccessible(true);
+                getPackage.setAccessible(true);
+                definePackage.setAccessible(true);
                 return null;
             }
         });
@@ -49,6 +58,16 @@ final class Injector {
             // nothing to do
         }
         try {
+            int packIndex = className.lastIndexOf('.');
+            if (packIndex != -1) {
+                String pkgname = className.substring(0, packIndex);
+                // Check if package already loaded.
+                Package pkg = (Package)getPackage.invoke(cl, pkgname);
+                if (pkg == null) {
+                    definePackage.invoke(cl, pkgname, null, null, null, null, null, null, null);
+                }
+            }
+
             Class c = (Class)defineClass.invoke(cl,className.replace('/','.'),image,0,image.length);
             resolveClass.invoke(cl, c);
             return c;
