c16737936af5780f75dcb5a0c276c294985f3731
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2010-07-21 07:20:10 +0000

    Adding AsyncServlet sample.

---
+++ jaxws-ri/samples/asyncservice/Readme.txt
@@ -0,0 +1,39 @@
+asyncservice sample demonstrates JAX-WS RI specific endpoint to achieve
+server side asynchrony. This sample requires Servlet 3.0 API supported container like Glassfish 3.0.
+
+This endpint implementation is similar to that of the asyncprovider sample. Notice the use of
+ servlet 3.0 feature in web.xml, The servlet com.sun.xml.ws.transport.http.servlet.WSServlet is declared as async
+ supported through <async-supported>true</async-supported>. With this the web service can take advantage of the async
+ capabilities by releasing the request thread and sending response when ready. Unlike in the asyncprovider sample, no
+ container thread is blocked for the response to finish, there by effectively uses server resources to process more requests.
+
+* etc - configuration files
+    * hello_literal.wsdl wsdl file
+    * custom-client.xml client customization file
+    * custom-server.xml server customization file
+    * build.properties, deploy-targets.xml ant script to deploy the endpoint
+      war file
+    * sun-jaxws.xml deployment descriptor for web container
+* src source files
+    * client/AsyncClient.java - client application
+    * server/AsyncHelloImpl.java - server implementation
+
+* wsimport ant task is run to compile etc/hello_literal.wsdl
+    * generates
+      SEI - Hello
+      service class - Hello_Service
+
+* To run
+    * set JAXWS_HOME to the JAX-WS installation directory
+    * ant clean server - runs wsimport to compile hello_literal.wsdl and
+      generate server side artifacts and does the deployment
+    * ant clean client run - runs wsimport on the published wsdl by the deployed
+      endpoint, compiles the generated artifacts and the client application
+      then executes it.
+
+* Prerequisite
+    * Requires Servlet 3.0 API supported container.
+
+Refer to the Prerequisites defined in samples/docs/index.html.
+
+We appreciate your feedback, please send it to users@jax-ws.dev.java.net.
---
+++ jaxws-ri/samples/asyncservice/src/asyncservice/server/HelloAsyncImpl.java
@@ -0,0 +1,122 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ * 
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ * 
+ * Contributor(s):
+ * 
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package asyncservice.server;
+
+import com.sun.xml.ws.api.server.AsyncProvider;
+import com.sun.xml.ws.api.server.AsyncProviderCallback;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.WebServiceException;
+import javax.xml.ws.WebServiceProvider;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+@WebServiceProvider(
+    wsdlLocation="WEB-INF/wsdl/hello_literal.wsdl",
+    targetNamespace="urn:test",
+    serviceName="Hello")
+
+public class HelloAsyncImpl implements AsyncProvider<Source> {
+    static String body  = "<HelloResponse xmlns=\"urn:test:types\"><argument xmlns=\"\">%s</argument><extra xmlns=\"\">%s</extra></HelloResponse>";
+    private static final JAXBContext jaxbContext = createJAXBContext();
+    private int bodyIndex;
+
+    /*
+    public javax.xml.bind.JAXBContext getJAXBContext(){
+        return jaxbContext;
+    } */
+
+    private static javax.xml.bind.JAXBContext createJAXBContext(){
+        try{
+            return javax.xml.bind.JAXBContext.newInstance(ObjectFactory.class);
+        }catch(javax.xml.bind.JAXBException e){
+            throw new WebServiceException(e.getMessage(), e);
+        }
+    }
+
+    private Hello_Type recvBean(Source source) throws Exception {
+        System.out.println("**** recvBean ******");
+        return (Hello_Type)jaxbContext.createUnmarshaller().unmarshal(source);
+    }
+
+    private Source sendSource(String arg, String extra) {
+        System.out.println("**** sendSource ******");
+        String response = String.format(body,arg,extra);
+        return new StreamSource(
+            new ByteArrayInputStream(response.getBytes()));
+    }
+
+    public void invoke(Source source, AsyncProviderCallback<Source> cbak, WebServiceContext ctxt) {
+        System.out.println("**** Received in AsyncService Impl ******");
+		try {
+			Hello_Type hello = recvBean(source);
+			String arg = hello.getArgument();
+			new Thread(new RequestHandler(cbak, hello)).start();
+
+		} catch(Exception e) {
+            throw new WebServiceException("Endpoint failed", e);
+		}
+    }
+
+	private class RequestHandler implements Runnable {
+		final AsyncProviderCallback<Source> cbak;
+		final Hello_Type hello;
+
+		public RequestHandler(AsyncProviderCallback<Source> cbak, Hello_Type hello) {
+			this.cbak = cbak;
+			this.hello = hello;
+		}
+
+		public void run() {
+			try {
+				Thread.sleep(10000);
+			} catch(InterruptedException ie) {
+				cbak.sendError(new WebServiceException("Interrupted..."));
+				return;
+			}
+			try {
+				cbak.send(sendSource(hello.getArgument(),hello.getExtra()));
+			} catch(Exception e) {
+				cbak.sendError(new WebServiceException(e));
+			}
+		}
+	}
+
+}
