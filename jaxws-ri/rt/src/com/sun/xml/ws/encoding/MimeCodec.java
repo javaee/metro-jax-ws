fc4edb82dc5c643cc0d51946559e396eb58e0f58
Jitendra Kotamraju <jitendra.kotamraju@oracle.com>
2010-10-12 23:37:32 +0000

    [JAX-WS-643] If there is a DCH in CommandMap, don't overwrite with JAX-WS DCHs Issue number: Obtained from: Submitted by: Reviewed by:

--- jaxws-ri/rt/src/com/sun/xml/ws/encoding/MimeCodec.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/encoding/MimeCodec.java
@@ -80,14 +80,22 @@ abstract class MimeCodec implements Codec {
             if (map instanceof MailcapCommandMap) {
                 MailcapCommandMap mailMap = (MailcapCommandMap) map;
                 String hndlrStr = ";;x-java-content-handler=";
-                mailMap.addMailcap(
-                    "text/xml" + hndlrStr + XmlDataContentHandler.class.getName());
-                mailMap.addMailcap(
-                    "application/xml" + hndlrStr + XmlDataContentHandler.class.getName());
-                mailMap.addMailcap(
-                    "image/*" + hndlrStr + ImageDataContentHandler.class.getName());
-                mailMap.addMailcap(
-                    "text/plain" + hndlrStr + StringDataContentHandler.class.getName());
+                if (map.createDataContentHandler("text/xml") == null) {
+                    mailMap.addMailcap(
+                        "text/xml" + hndlrStr + XmlDataContentHandler.class.getName());
+                }
+                if (map.createDataContentHandler("application/xml") == null) {
+                    mailMap.addMailcap(
+                        "application/xml" + hndlrStr + XmlDataContentHandler.class.getName());
+                }
+                if (map.createDataContentHandler("image/*") == null) {
+                    mailMap.addMailcap(
+                        "image/*" + hndlrStr + ImageDataContentHandler.class.getName());
+                }
+                if (map.createDataContentHandler("text/plain") == null) {
+                    mailMap.addMailcap(
+                        "text/plain" + hndlrStr + StringDataContentHandler.class.getName());
+                }
             }
         } catch (Throwable t) {
             // ignore the exception.
