4af03fcd2fd51a6d7520cfaf2ed6f470e4ca0fc5
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2010-11-12 23:20:02 +0000

    JAX-WS-202:Default CatalogResolver implementation is broken as it depends on CatalogManager.getCatalog() which will always create a new one when useStaticCatalog is false. Workaround it by passing the overriden CatalogManager to the CatalogResolver.

--- jaxws-ri/rt/src/com/sun/xml/ws/util/xml/XmlUtil.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/xml/XmlUtil.java
@@ -37,6 +37,7 @@
 package com.sun.xml.ws.util.xml;
 
 import com.sun.istack.Nullable;
+import com.sun.org.apache.xml.internal.resolver.Catalog;
 import com.sun.org.apache.xml.internal.resolver.CatalogManager;
 import com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver;
 import com.sun.xml.ws.server.ServerRtException;
@@ -261,18 +262,21 @@ public class XmlUtil {
     /*
     * Gets an EntityResolver using XML catalog
     */
-    public static EntityResolver createEntityResolver(@Nullable URL catalogUrl) {
+     public static EntityResolver createEntityResolver(@Nullable URL catalogUrl) {
         // set up a manager
         CatalogManager manager = new CatalogManager();
         manager.setIgnoreMissingProperties(true);
+        // Using static catalog may  result in to sharing of the catalog by multiple apps running in a container
+        manager.setUseStaticCatalog(false);
+        Catalog catalog = manager.getCatalog();
         try {
             if (catalogUrl != null) {
-                manager.getCatalog().parseCatalog(catalogUrl);
+                catalog.parseCatalog(catalogUrl);
             }
         } catch (IOException e) {
             throw new ServerRtException("server.rt.err",e);
         }
-        return new CatalogResolver(manager);
+        return workaroundCatalogResolver(catalog);
     }
 
     /**
@@ -283,10 +287,12 @@ public class XmlUtil {
         // set up a manager
         CatalogManager manager = new CatalogManager();
         manager.setIgnoreMissingProperties(true);
-
+        // Using static catalog may  result in to sharing of the catalog by multiple apps running in a container
+        manager.setUseStaticCatalog(false);
         // parse the catalog
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
         Enumeration<URL> catalogEnum;
+        Catalog catalog = manager.getCatalog();
         try {
             if (cl == null) {
                 catalogEnum = ClassLoader.getSystemResources("META-INF/jax-ws-catalog.xml");
@@ -296,11 +302,33 @@ public class XmlUtil {
 
             while(catalogEnum.hasMoreElements()) {
                 URL url = catalogEnum.nextElement();
-                manager.getCatalog().parseCatalog(url);
+                catalog.parseCatalog(url);
             }
         } catch (IOException e) {
             throw new WebServiceException(e);
         }
+
+        return workaroundCatalogResolver(catalog);
+    }
+
+    /**
+     *  Default CatalogResolver implementation is broken as it depends on CatalogManager.getCatalog() which will always create a new one when
+     *  useStaticCatalog is false.
+     *  This returns a CatalogResolver that uses the catalog passed as parameter.
+     * @param catalog
+     * @return  CatalogResolver
+     */
+    private static CatalogResolver workaroundCatalogResolver(final Catalog catalog) {
+        // set up a manager
+        CatalogManager manager = new CatalogManager() {
+            @Override
+            public Catalog getCatalog() {
+                return catalog;
+            }
+        };
+        manager.setIgnoreMissingProperties(true);
+        // Using static catalog may  result in to sharing of the catalog by multiple apps running in a container
+        manager.setUseStaticCatalog(false);
 
         return new CatalogResolver(manager);
     }
