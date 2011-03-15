a05f12593a79c38f1440c13b66f1a9efce0df7f4
Shih-Chang Chen <shih-chang.chen@oracle.com>
2011-03-15 15:25:03 +0000

    eclipselink plugin test

---
+++ jaxws-ri/eclipselink_jaxb/test/com/sun/xml/ws/eclipselink/HelloImpl.java
@@ -0,0 +1,5 @@
+package com.sun.xml.ws.eclipselink;
+
+public class HelloImpl {
+	public String echoS(String s) { return s;};
+}
---
+++ jaxws-ri/eclipselink_jaxb/test/com/sun/xml/ws/eclipselink/HelloPort.java
@@ -0,0 +1,5 @@
+package com.sun.xml.ws.eclipselink;
+
+public interface HelloPort {
+	public String echoS(String s);
+}
