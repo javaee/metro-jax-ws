85a6c5dc1a0166ae3d18c35ad75597d631a09d2a
Rama Pulavarthi <rama.pulavarthi@oracle.com>
2011-07-01 00:02:41 +0000

    Fixing the tests to write the EPR and read it back again to varify the validity of the buffered xml infoset.

--- jaxws-unit/testcases/whitebox/wsdlparser/client/EPRWriteNReadTest.java
+++ jaxws-unit/testcases/whitebox/wsdlparser/client/EPRWriteNReadTest.java
@@ -41,6 +41,7 @@
 package whitebox.wsdlparser.client;
 
 import com.sun.xml.ws.test.VersionRequirement;
+import com.sun.xml.ws.util.ByteArrayBuffer;
 import com.sun.xml.ws.wsdl.parser.RuntimeWSDLParser;
 import com.sun.xml.ws.api.model.wsdl.*;
 import com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver;
@@ -50,9 +51,13 @@ import org.xml.sax.EntityResolver;
 import com.sun.xml.ws.api.addressing.WSEndpointReference;
 
 import javax.xml.namespace.QName;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.*;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.EndpointReference;
+import javax.xml.ws.wsaddressing.W3CEndpointReference;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -60,26 +65,20 @@ import java.util.Enumeration;
 
 /**
  * Tests to verify that the EndpointReference inside wsdl:port is captured correctly so that it can be used as the destination EPR.
- * Tests verify that the ns declarations on the golbal/parent elements is captured in to the EPR infoset.
+ * Tests verify that the ns declarations on the golbal/parent elements is captured in to the EPR infoset by writing the
+ * infoset and reading it back again
  *
  * @author Rama Pulavarthi
  */
 @VersionRequirement(since = "2.2.6")
-public class EPREWithNSDeclarationsTest extends TestCase {
+public class EPRWriteNReadTest extends TestCase {
     public void test1() throws Exception {
         URL fileurl = getResource("hello_literal_identity1.wsdl");
         WSDLModel doc = RuntimeWSDLParser.parse(fileurl, new StreamSource(fileurl.toExternalForm()), getResolver(), true, null);
         WSDLService service = doc.getService(new QName("urn:test", "Hello"));
         WSDLPort port = service.getFirstPort();
         WSEndpointReference wsepr = port.getExtension(WSEndpointReference.class);
-        QName q = new QName("http://schemas.xmlsoap.org/ws/2006/02/addressingidentity", "Identity");
-        WSEndpointReference.EPRExtension eprExtn = wsepr.getEPRExtension(q);
-        XMLStreamReader xsr = eprExtn.readAsXMLStreamReader();
-        if (xsr.getEventType() == XMLStreamConstants.START_DOCUMENT)
-            xsr.next();
-        assertEquals(q.getNamespaceURI(), xsr.getNamespaceURI());
-        assertEquals(q.getLocalPart(), xsr.getLocalName());
-
+        verifyEPR(wsepr);
     }
 
     public void test2() throws Exception {
@@ -88,14 +87,7 @@ public class EPREWithNSDeclarationsTest extends TestCase {
         WSDLService service = doc.getService(new QName("urn:test", "Hello2"));
         WSDLPort port = service.getFirstPort();
         WSEndpointReference wsepr = port.getExtension(WSEndpointReference.class);
-        QName q = new QName("http://schemas.xmlsoap.org/ws/2006/02/addressingidentity", "Identity");
-        WSEndpointReference.EPRExtension eprExtn = wsepr.getEPRExtension(q);
-        XMLStreamReader xsr = eprExtn.readAsXMLStreamReader();
-        if (xsr.getEventType() == XMLStreamConstants.START_DOCUMENT)
-            xsr.next();
-        assertEquals(q.getNamespaceURI(), xsr.getNamespaceURI());
-        assertEquals(q.getLocalPart(), xsr.getLocalName());
-
+        verifyEPR(wsepr);
     }
 
     public void test3() throws Exception {
@@ -104,14 +96,30 @@ public class EPREWithNSDeclarationsTest extends TestCase {
         WSDLService service = doc.getService(new QName("urn:test", "Hello2"));
         WSDLPort port = service.getFirstPort();
         WSEndpointReference wsepr = port.getExtension(WSEndpointReference.class);
-        QName q = new QName("http://schemas.xmlsoap.org/ws/2006/02/addressingidentity", "Identity");
-        WSEndpointReference.EPRExtension eprExtn = wsepr.getEPRExtension(q);
-        XMLStreamReader xsr = eprExtn.readAsXMLStreamReader();
-        if (xsr.getEventType() == XMLStreamConstants.START_DOCUMENT)
-            xsr.next();
-        assertEquals(q.getNamespaceURI(), xsr.getNamespaceURI());
-        assertEquals(q.getLocalPart(), xsr.getLocalName());
+        verifyEPR(wsepr);
+    }
 
+    private static void verifyEPR( WSEndpointReference wsepr) throws Exception {
+        ByteArrayBuffer bytebuffer = new ByteArrayBuffer();
+        XMLStreamWriter w = XMLOutputFactory.newInstance().createXMLStreamWriter(bytebuffer);
+        System.out.println("----------------------------------------------------------------------");
+        w.writeStartDocument();
+        wsepr.writeTo("EndpointReference",w);
+        w.writeEndDocument();
+        System.out.println(bytebuffer.toString());
+
+        //verify the validity of infoset by reading it as EPR back again
+        EndpointReference epr = W3CEndpointReference.readFrom(new StreamSource(bytebuffer.newInputStream()));
+
+        //Verify it good old way
+        XMLStreamReader xsr = XMLInputFactory.newInstance().createXMLStreamReader(new StreamSource(bytebuffer.newInputStream()));
+        QName q = new QName("http://schemas.xmlsoap.org/ws/2006/02/addressingidentity", "Identity");
+        while(xsr.hasNext()) {
+            xsr.next();
+            if(xsr.getEventType() == XMLStreamConstants.START_ELEMENT && xsr.getLocalName().equals("Identity")) {
+                assertEquals(q.getNamespaceURI(), xsr.getNamespaceURI());
+            }
+        }
     }
 
     private static EntityResolver getResolver() {
