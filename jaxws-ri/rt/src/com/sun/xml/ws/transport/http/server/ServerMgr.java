f4d28e0bd109f41fa52a1eff736e931bef157e0d
lali_oracle <lali_oracle>
2011-08-03 08:17:57 +0000

    JAX-WS-975: Fix for constructing the actual inet address in ServerMgr to avoid NPE

--- jaxws-ri/rt/src/com/sun/xml/ws/transport/http/server/ServerMgr.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/transport/http/server/ServerMgr.java
@@ -102,6 +102,11 @@ final class ServerMgr {
                     logger.fine("Creating HTTP Context at = "+path);
                     HttpContext context = server.createContext(path);
                     server.start();
+                    if (port == 0) {
+                        //A port number of zero will let the system pick up an ephemeral port in a bind operation.
+                        //Get the actual inetAddress from server, which has the bound port instead of supplied port number of zero.
+                        inetAddress = server.getAddress();
+                    }
                     logger.fine("HTTP server started = "+inetAddress);
                     state = new ServerState(server);
                     servers.put(inetAddress, state);
