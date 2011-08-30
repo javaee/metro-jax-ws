fe491d4440ddee723c00fbdcc1fa2630b8ea84c2
Lukas Jungmann <lukas.jungmann@oracle.com>
2011-08-30 08:45:23 +0000

    JAX_WS-989 - Bug 9170825 - JWSC ANT TASK TASK FAILS TO EXECUTE WITH OUT OF MEMORY.

--- jaxws-ri/tools/wscompile/src/com/sun/tools/ws/ant/Apt.java
+++ jaxws-ri/tools/wscompile/src/com/sun/tools/ws/ant/Apt.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -324,6 +324,13 @@ public class Apt extends Task {
             cmd.createArgument().setValue(getTarget());
         }
         
+        //if fork, these arguments will appear twice
+        if (!fork) {
+            for (Jvmarg jvmarg : jvmargs) {
+                cmd.createArgument().setValue("-J" + jvmarg.getValue());
+            }
+        }
+
         for (Option option : options) {
             cmd.createArgument().setValue("-A" + option.getKey() + "=" + option.getValue());
         }
---
+++ jaxws-ri/tools/wscompile/test/com/sun/tools/ws/ant/AptTest.java
@@ -0,0 +1,135 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.tools.ws.ant;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.TestCase;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.Commandline;
+import org.apache.tools.ant.types.Path;
+
+/**
+ *
+ * @author Lukas Jungmann
+ */
+public class AptTest extends TestCase {
+
+    public void testSetupAptArgs() throws URISyntaxException {
+        Apt apt = getApt();
+        String vmArg = "-Xmx2048M";
+        String testedMethod = "setupAptArgs";
+        apt.setFork(false);
+        apt.createJvmarg().setValue(vmArg);
+        Commandline cmd = getCommandLine(testedMethod, apt);
+        assertTrue("Option for runtime system must be prefixed with '-J'", containsArgument(cmd.getArguments(), "-J" + vmArg));
+        apt.setFork(true);
+        cmd = getCommandLine(testedMethod, apt);
+        assertFalse("Option for forked process should not be prefixed with '-J'", containsArgument(cmd.getArguments(), "-J" + vmArg));
+    }
+
+    private Apt getApt() {
+        Apt apt = new Apt();
+        Project p = new Project();
+        p.init();
+        Path src = new Path(p);
+        src.setPath(System.getProperty("java.io.tmpdir"));
+        apt.setSourcepath(src);
+        return apt;
+    }
+    
+    private static Commandline getCommandLine(String name, Object a) {
+        final Method m = getMethod(name);
+        assertNotNull(m);
+        try {
+            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+
+                public Void run() {
+                    m.setAccessible(true);
+                    return null;
+                }
+            });
+
+            return (Commandline) m.invoke(a);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(AptTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(AptTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(AptTest.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (m != null && m.isAccessible()) {
+                AccessController.doPrivileged(new PrivilegedAction<Void>() {
+
+                    public Void run() {
+                        m.setAccessible(false);
+                        return null;
+                    }
+                });
+            }
+        }
+        return null;
+    }
+
+    private static Method getMethod(String name) {
+        try {
+            return Apt.class.getDeclaredMethod(name);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(AptTest.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(AptTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    private static boolean containsArgument(String[] args, String arg) {
+        for (String s : args) {
+            if (s.equals(arg)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
