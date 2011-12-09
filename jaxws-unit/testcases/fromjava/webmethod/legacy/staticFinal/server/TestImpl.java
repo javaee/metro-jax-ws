8f1a4e3094451e68dc3d7f6f2c92a8f009037c8b
Iaroslav Savytskyi <iaroslav.savytskyi@oracle.com>
2011-12-09 16:16:47 +0000

    Adding a test case for issue: 1024 - legacy webMethod computation

---
+++ jaxws-unit/testcases/fromjava/webmethod/legacy/staticFinal/server/TestImpl.java
@@ -0,0 +1,48 @@
+package fromjava.webmethod.legacy.staticFinal.server;
+
+import javax.jws.WebService;
+import java.rmi.Remote;
+
+/**
+ * User: Iaroslav Savytskyi
+ * Date: 01.12.11
+ */
+@WebService
+public class TestImpl {
+
+    // This is WebMethod
+    public String method1(String str) {
+        return str;
+    }
+
+    // This is not a WebMethod since static
+    public static String method2(String str) {
+        return str;
+    }
+
+    // This is not a WebMethod since final
+    public final String method3(String str) {
+        return str;
+    }
+
+    // This is not a WebMethod since package access
+    String method4(String str) {
+        return str;
+    }
+
+    // This is not a WebMethod since protected access
+    protected String method5(String str) {
+        return str;
+    }
+
+    // This is not a WebMethod since private access
+    private String method6(String str) {
+        return str;
+    }
+
+    // This method shouldn't be validated
+    protected Remote method7(String str) {
+        return null;
+    }
+
+}
