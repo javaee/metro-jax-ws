881240c9627a35455ff08a629657cda4a864883b
Martin Grebac <martin.grebac@oracle.com>
2012-02-17 09:50:47 +0000

    minor, unused PropertySet reference

--- jaxws-ri/rt/src/com/sun/xml/ws/util/ReadOnlyPropertyException.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/util/ReadOnlyPropertyException.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -39,8 +39,6 @@
  */
 
 package com.sun.xml.ws.util;
-
-import com.sun.xml.ws.api.PropertySet;
 
 /**
  * Used to indicate that {@link PropertySet#put(String, Object)} failed
