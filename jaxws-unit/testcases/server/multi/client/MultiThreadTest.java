e637914914ed8b12622b19d0697b2dc60236e5ab
Miroslav Kos <miroslav.kos@oracle.com>
2012-03-15 13:00:19 +0000

    http://java.net/jira/browse/JAX_WS-1049 - Reading from BindingProvider's RequestContext causes "SOAPAction" http header to go blank

--- jaxws-unit/testcases/server/multi/client/MultiThreadTest.java
+++ jaxws-unit/testcases/server/multi/client/MultiThreadTest.java
@@ -9,12 +9,16 @@
 
 package server.multi.client;
 
-import java.io.*;
-import java.util.concurrent.*;
+import junit.framework.TestCase;
+
+import javax.xml.ws.BindingProvider;
+import java.util.Map;
 import java.util.Random;
-import junit.framework.*;
-import javax.xml.soap.*;
-import javax.xml.namespace.QName;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -24,6 +28,7 @@ import javax.xml.namespace.QName;
 public class MultiThreadTest extends TestCase {
     
     private static final int NO_THREADS = 20;
+    private static final int NO_REQS = 50000;
     private int noReqs = 0;
     private int noResps = 0;
     private final HelloPortType stub;
@@ -32,11 +37,19 @@ public class MultiThreadTest extends TestCase {
         super(name);
         HelloService service = new HelloService();
         stub = service.getHelloPort();
+
+        // initialize
+        Map<String,Object> ctx = ((BindingProvider) stub).getRequestContext();
+        ctx.put("whatever", "whatever");
+        ctx.put("whatever2", "whatever2");
+
+        // this turns RequestContext to fallback mode
+        ctx.keySet();
     }
 
     public void testMultiThread() throws Exception {
         synchronized(this) {
-            noReqs = 50000; noResps = 0;    
+            noReqs = NO_REQS; noResps = 0;
         }
         Thread[] threads = new Thread[NO_THREADS];
         for(int i=0; i < NO_THREADS; i++) {
@@ -86,7 +99,7 @@ public class MultiThreadTest extends TestCase {
         ExecutorService service = new ThreadPoolExecutor(NO_THREADS/2, NO_THREADS,
             30L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()); 
         synchronized(this) {
-            noReqs = 50000; noResps = 0; 
+            noReqs = NO_REQS; noResps = 0;
         }
         doTestWithThreadPool(service, noReqs);
         service.shutdown();
@@ -99,7 +112,7 @@ public class MultiThreadTest extends TestCase {
     public void testFixedThreadPool() throws Exception {
         ExecutorService service = Executors.newFixedThreadPool(NO_THREADS);
         synchronized(this) {
-            noReqs = 50000; noResps = 0; 
+            noReqs = NO_REQS; noResps = 0;
         }
         doTestWithThreadPool(service, noReqs);
         service.shutdown();
@@ -112,7 +125,7 @@ public class MultiThreadTest extends TestCase {
     public void testCachedThreadPool() throws Exception {
         ExecutorService service = Executors.newCachedThreadPool();
         synchronized(this) {
-            noReqs = 50; noResps = 0; 
+            noReqs = 50; noResps = 0;
         }
         doTestWithThreadPool(service, noReqs);
         service.shutdown();
