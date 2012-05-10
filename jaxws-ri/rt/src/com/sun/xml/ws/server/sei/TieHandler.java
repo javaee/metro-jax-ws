8fe7f040598320ddb62482f24840be2fa6949f76
Martin Grebac <martin.grebac@oracle.com>
2012-05-10 12:34:35 +0000

    Additional update required after: "Added argument checking to default MessageContextFactory implementation." rev 13102 Otherwise breaks Metro RM.

--- jaxws-ri/rt/src/com/sun/xml/ws/server/sei/TieHandler.java
+++ jaxws-ri/rt/src/com/sun/xml/ws/server/sei/TieHandler.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -337,10 +337,10 @@ final public class TieHandler implements EndpointCallBridge {
 	}
 
 	public Packet serializeResponse(JavaCallInfo call) {
-		Message msg = this.createResponse(call);
-        Packet p = (Packet)packetFactory.createContext(msg);
-        p.setState(Packet.State.ServerResponse);
-        return p;
+            Message msg = this.createResponse(call);
+            Packet p = (msg == null) ? (Packet)packetFactory.createContext() : (Packet)packetFactory.createContext(msg);
+            p.setState(Packet.State.ServerResponse);
+            return p;
 	}
 	
     public JavaMethod getOperationModel() {
