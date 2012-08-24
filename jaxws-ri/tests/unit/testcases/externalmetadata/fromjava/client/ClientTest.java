a564784a1f8a7203ccb67765e430345ba0ffefcb
Miroslav Kos <miroslav.kos@oracle.com>
2012-08-24 14:37:58 +0000

    added documentation, samples and tests for external metadata

---
+++ jaxws-ri/tests/unit/testcases/externalmetadata/fromjava/client/ClientTest.java
@@ -0,0 +1,111 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package externalmetadata.fromjava.client;
+
+import junit.framework.TestCase;
+import org.jvnet.ws.databinding.ExternalMetadataFeature;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceFeature;
+import javax.xml.ws.handler.MessageContext;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static javax.xml.ws.BindingProvider.SOAPACTION_USE_PROPERTY;
+
+/**
+ * Test for JAX_WS-1049: problem with filling packet from {@link com.sun.xml.ws.client.RequestContext} - setting different properties to
+ * {@link com.sun.xml.ws.client.RequestContext} and checking on server side that those were applied correctly (specifically SOAPAction +
+ * custom HTTP headers)
+ *
+ * @author Miroslav Kos (miroslav.kos at oracle.com)
+ */
+public class ClientTest extends TestCase {
+
+    public void test() throws SOAPException, IOException {
+
+        // -- TEST different SOAPACTION properties in RequestContext
+        File file = new File("testcases/externalmetadata/fromjava/client/external-wsdl-customization-client.xml");
+        WebServiceFeature feature = ExternalMetadataFeature.builder().addFiles(file).build();
+        FAKENAME port = new EchoImplService().getFAKENAMEPort(feature);
+        Map<String, Object> requestContext = ((BindingProvider) port).getRequestContext();
+
+        //THIS next line is causing the SOAPACTION http header to go blank, if fix JAX_WS-1049 not applied ...
+        requestContext.keySet();
+
+        port.doSomething();
+
+        // -- TEST dispatch: http://java.net/jira/browse/JAX_WS-1014 : Bug <12883765>
+        String jaxwsMsg = "<?xml version='1.0' encoding='UTF-8'?>" +
+                "<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<S:Body><doSomething xmlns=\"overriden-target-namespace\"/></S:Body></S:Envelope>";
+
+        EchoImplService service = new EchoImplService();
+        Dispatch<SOAPMessage> dispatch = service.createDispatch(new QName("overriden-target-namespace", "FAKE-NAMEPort"), SOAPMessage.class, Service.Mode.MESSAGE);
+
+        Map<String, List> headersMap = new HashMap<String, List>();
+        headersMap.put("X-ExampleHeader2", Collections.singletonList("Value"));
+        Map<String, Object> context = dispatch.getRequestContext();
+
+        context.put(MessageContext.HTTP_REQUEST_HEADERS, headersMap);
+        context.put(SOAPACTION_USE_PROPERTY, Boolean.TRUE);
+
+        MimeHeaders mhs = new MimeHeaders();
+
+        mhs.addHeader("My-Content-Type", "text/xml");
+        mhs.addHeader("SOAPAction", "overridenInputAction");
+
+        SOAPMessage msg = MessageFactory.newInstance().createMessage(mhs, new ByteArrayInputStream(jaxwsMsg.getBytes()));
+        dispatch.invoke(msg);
+    }
+
+}
---
+++ jaxws-ri/tests/unit/testcases/externalmetadata/fromwsdl/client/ClientTest.java
@@ -0,0 +1,98 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package externalmetadata.fromwsdl.client;
+
+import junit.framework.TestCase;
+import org.jvnet.ws.databinding.ExternalMetadataFeature;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static javax.xml.ws.BindingProvider.SOAPACTION_USE_PROPERTY;
+
+/**
+ * TODO-Miran: javadoc
+ *
+ * @author Miroslav Kos (miroslav.kos at oracle.com)
+ */
+public class ClientTest extends TestCase {
+
+    public void test() throws SOAPException, IOException {
+
+        ServiceName port = new ExternalMetadataService().getServiceNamePort();
+        port.doSomething();
+
+        // -- TEST dispatch: http://java.net/jira/browse/JAX_WS-1014 : Bug <12883765>
+        String jaxwsMsg = "<?xml version='1.0' encoding='UTF-8'?>" +
+                "<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<S:Body><doSomething xmlns=\"my-target-namespace\"/></S:Body></S:Envelope>";
+
+        ExternalMetadataService service = new ExternalMetadataService();
+        Dispatch<SOAPMessage> dispatch = service.createDispatch(new QName("my-target-namespace", "ServiceNamePort"), SOAPMessage.class, Service.Mode.MESSAGE);
+
+        Map<String, List> headersMap = new HashMap<String, List>();
+        headersMap.put("X-ExampleHeader2", Collections.singletonList("Value"));
+        Map<String, Object> context = dispatch.getRequestContext();
+
+        context.put(MessageContext.HTTP_REQUEST_HEADERS, headersMap);
+        context.put(SOAPACTION_USE_PROPERTY, Boolean.TRUE);
+
+        MimeHeaders mhs = new MimeHeaders();
+
+        mhs.addHeader("My-Content-Type", "text/xml");
+        mhs.addHeader("SOAPAction", "myInputAction");
+
+        SOAPMessage msg = MessageFactory.newInstance().createMessage(mhs, new ByteArrayInputStream(jaxwsMsg.getBytes()));
+        dispatch.invoke(msg);
+    }
+
+}
