32997e32367ef27c6ffa8ea4cca536ce91a56815
Lukas Jungmann <lukas.jungmann@oracle.com>
2012-09-10 13:46:36 +0000

    BUG 14492194 - CDATA SEGMENT IS MISSING (backport fix for bug 12986806 to 226 branch)

--- jaxws-ri/rt/test/com/sun/xml/ws/message/stream/StreamMessageTest.java
+++ jaxws-ri/rt/test/com/sun/xml/ws/message/stream/StreamMessageTest.java
@@ -1,14 +1,14 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
@@ -236,6 +236,26 @@ public class StreamMessageTest extends TestCase {
         xsr.next();
         xsr.next();
         assertEquals("http://schemas.xmlsoap.org/soap/envelope/", xsr.getNamespaceURI("ns4"));
+    }
+
+    public void testCData() throws Exception {
+        String soap18Msg = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+"<soapenv:Body>" +
+    "<echoRequest xmlns=\"http://test.oracle.com/xsd\">" +
+      "<arg0>outside cdata <![CDATA[<data>inside cdata</data>]]></arg0>" +
+    "</echoRequest>" +
+  "</soapenv:Body>" +
+"</soapenv:Envelope>";
+        Message message = useStreamCodec(soap18Msg);
+        Source source = message.readPayloadAsSource();
+        InputStream is = getInputStream(source);
+        XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(is);
+        reader.next();
+        while (reader.getEventType() == XMLStreamReader.START_ELEMENT) {
+            reader.next();
+        }
+        String text = new String(reader.getTextCharacters(), reader.getTextStart(), reader.getTextLength());
+        assertEquals("outside cdata <data>inside cdata</data>", text);
     }
 
 /*
