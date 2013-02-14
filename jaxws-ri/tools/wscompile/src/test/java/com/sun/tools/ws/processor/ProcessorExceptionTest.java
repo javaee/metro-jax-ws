bbd5afafb02be5ceaf69fc8a737e612e599c6e79
Lukas Jungmann <lukas.jungmann@oracle.com>
2013-02-14 13:59:57 +0000

    test should really test something

--- jaxws-ri/tools/wscompile/src/test/java/com/sun/tools/ws/processor/ProcessorExceptionTest.java
+++ jaxws-ri/tools/wscompile/src/test/java/com/sun/tools/ws/processor/ProcessorExceptionTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -37,8 +37,8 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
 package com.sun.tools.ws.processor;
+
 import com.sun.istack.localization.Localizer;
 import junit.framework.TestCase;
 
@@ -46,9 +46,11 @@ import junit.framework.TestCase;
  * @author Rama Pulavarthi
  */
 public class ProcessorExceptionTest extends TestCase {
+
     public void testLocalizability() {
-     ProcessorException pe = new ProcessorException("Try localizing me");
+        String msg = "Try localizing me";
+        ProcessorException pe = new ProcessorException(msg);
         Localizer localizer = new Localizer();
-        localizer.localize(pe);
+        assertEquals(msg, localizer.localize(pe));
     }
 }
