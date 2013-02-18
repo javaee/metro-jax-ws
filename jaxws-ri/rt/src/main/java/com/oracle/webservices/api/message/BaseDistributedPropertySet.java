aa339f95aefd30f63ec0f13528bc7bbba055f0ac
Ryan Eberhard <ryan.eberhard@oracle.com>
2013-02-18 22:48:07 +0000

    merge 13767

--- jaxws-ri/rt/src/main/java/com/oracle/webservices/api/message/BaseDistributedPropertySet.java
+++ jaxws-ri/rt/src/main/java/com/oracle/webservices/api/message/BaseDistributedPropertySet.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -243,6 +243,18 @@ public abstract class BaseDistributedPropertySet extends BasePropertySet impleme
                 size += child.asMap().size();
             }
             return size;
+        }
+        
+        @Override
+        public boolean containsKey(Object key) {
+            if (viewthis.containsKey(key))
+                return true;
+            for (PropertySet child : satellites.values()) {
+                if (child.containsKey(key)) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         @Override
--- jaxws-ri/rt/src/main/java/com/oracle/webservices/api/message/BasePropertySet.java
+++ jaxws-ri/rt/src/main/java/com/oracle/webservices/api/message/BasePropertySet.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -403,7 +403,7 @@ public abstract class BasePropertySet implements PropertySet {
 
     @Override
     public final boolean containsKey(Object key) {
-        return get(key)!=null;
+        return getPropertyMap().containsKey(key);
     }
 
     /**
