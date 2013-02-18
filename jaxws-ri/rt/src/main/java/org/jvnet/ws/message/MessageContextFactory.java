ad8b27f4d42b8c11cf6bc3f4e50e43d562c5e437
Ryan Eberhard <ryan.eberhard@oracle.com>
2013-02-18 17:29:06 +0000

    MessageContextFactory updated missing from earlier stream decoder work

--- jaxws-ri/rt/src/main/java/org/jvnet/ws/message/MessageContextFactory.java
+++ jaxws-ri/rt/src/main/java/org/jvnet/ws/message/MessageContextFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -261,6 +261,12 @@ public abstract class MessageContextFactory extends com.oracle.webservices.api.m
 				throws IOException {
 			return wrap(mcf.createContext(in, contentType));
 		}
+
+        @Override
+        public MessageContext createContext(InputStream in, int length, String contentType)
+                throws IOException {
+            return wrap(mcf.createContext(in, contentType));
+        }
 
 		@Override
 		@Deprecated
