1b9f50cb5c8b624aa9b70de610932539f7ecb3d6
Iaroslav Savytskyi <iaroslav.savytskyi@oracle.com>
2013-02-21 17:53:38 +0000

    Fixing JAXB jars classpath

--- jaxws-ri/bundles/jaxws-ri/src/main/resources/bin/wsgen.bat
+++ jaxws-ri/bundles/jaxws-ri/src/main/resources/bin/wsgen.bat
@@ -64,7 +64,7 @@ set JAVA="%JAVA_HOME%\bin\java"
 goto LAUNCH
 
 :LAUNCH
-%JAVA% %WSGEN_OPTS% -jar "%JAXWS_HOME%\lib\jaxws-tools.jar:%JAXWS_HOME%\lib\jaxws-rt.jar:%JAXWS_HOME%\lib\jaxb-xjc.jar:%JAXWS_HOME%\lib\jaxws-jxc.jar:%JAXWS_HOME%\lib\jaxb-api.jar:%JAXWS_HOME%\lib\jaxws-core.jar:%JAXWS_HOME%\lib\jaxb-impl.jar" com.sun.tools.ws.WsGen %*
+%JAVA% %WSGEN_OPTS% -cp "%JAXWS_HOME%\lib\jaxws-tools.jar;%JAXWS_HOME%\lib\jaxws-rt.jar;%JAXWS_HOME%\lib\jaxb-xjc.jar;%JAXWS_HOME%\lib\jaxb-jxc.jar;%JAXWS_HOME%\lib\jaxb-api.jar;%JAXWS_HOME%\lib\jaxb-core.jar;%JAXWS_HOME%\lib\jaxb-impl.jar" com.sun.tools.ws.WsGen %*
 
 :END
 %COMSPEC% /C exit %ERRORLEVEL%
