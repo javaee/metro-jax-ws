d5d3b0484e68ba110c8234b3f01629601fb7591a
Martin Grebac <martin.grebac@oracle.com>
2013-03-06 17:29:32 +0000

    merge 13910 from 228 - org.jvnet removal from docs/samples

--- jaxws-ri/samples/src/main/samples/external-metadata-fromwsdl/src/externalmetadata/fromwsdl/server/J2SETester.java
+++ jaxws-ri/samples/src/main/samples/external-metadata-fromwsdl/src/externalmetadata/fromwsdl/server/J2SETester.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -52,8 +52,8 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.jvnet.ws.databinding.ExternalMetadataFeature;
-import org.jvnet.ws.databinding.ExternalMetadataFeature.Builder;
+import com.oracle.webservices.api.databinding.ExternalMetadataFeature;
+import com.oracle.webservices.api.databinding.ExternalMetadataFeature.Builder;
 
 /**
  * simple J2SE tester class for this sample
