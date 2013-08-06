15d42d71fa34e1da9dd00817406e4664c9d6abd0
Martin Grebac <martin.grebac@oracle.com>
2013-08-06 08:06:08 +0000

    - fix copyright header, unused import

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/api/server/Container.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/api/server/Container.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -42,7 +42,6 @@ package com.sun.xml.ws.api.server;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
