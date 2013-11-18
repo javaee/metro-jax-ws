5b803cb3ecedbec559ccda75794fc49aaad83f5e
deva.sagar@oracle.com <deva.sagar@oracle.com>
2013-11-18 11:15:07 -0500

    SAAJMessage copy() should materialize the message fully - added access() call at start of copy()

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/message/saaj/SAAJMessage.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/message/saaj/SAAJMessage.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -522,6 +522,7 @@ public class SAAJMessage extends Message {
      * the {@link com.sun.xml.ws.api.message.Message} implementation itself.
      */
     public Message copy() {
+        access();
         try {
             if (!parsedMessage) {
                 return new SAAJMessage(readAsSOAPMessage());
