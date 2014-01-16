2f7fba19581e6675bd1765bbb36d2cdfbcc15cec
Martin Grebac <martin.grebac@oracle.com>
2014-01-16 09:30:15 +0100

    Fix findbugs warning caused by merge from cpu.

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/client/sei/SEIStub.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/client/sei/SEIStub.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -182,8 +182,7 @@ public final class SEIStub extends Stub implements InvocationHandler {
         if (proxy == null || !Proxy.isProxyClass(proxy.getClass())) {
             throw new IllegalStateException("Passed object is not proxy!");
         }
-        Class<?> declaringClass = method.getDeclaringClass();
-        if (method == null || declaringClass == null
+        if (method == null || method.getDeclaringClass() == null
                 || Modifier.isStatic(method.getModifiers())) {
             throw new IllegalStateException("Invoking static method is not allowed!");
         }
