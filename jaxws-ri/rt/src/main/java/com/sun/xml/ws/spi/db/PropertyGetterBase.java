7ab9dd22a711d3f003dd2ff67f018f3095ed1114
Shih-Chang Chen <shih-chang.chen@oracle.com>
2014-02-18 12:09:21 -0800

    fix Boolean

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/spi/db/PropertyGetterBase.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/spi/db/PropertyGetterBase.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -61,7 +61,7 @@ public abstract class PropertyGetterBase implements PropertyGetter {
                 method.getName().length() > 3) {
                 return true;
             } else {
-                if (method.getReturnType().equals(boolean.class) &&
+                if ((method.getReturnType().equals(boolean.class) || method.getReturnType().equals(Boolean.class)) &&
                     method.getName().startsWith("is") &&
                     method.getName().length() > 2) {
                     return true;
