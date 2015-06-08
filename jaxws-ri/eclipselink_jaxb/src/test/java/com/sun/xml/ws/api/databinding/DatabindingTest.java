e5458ca513d94e8d4a7c51d6159941dcd0518a3d
Shih-Chang Chen <shih-chang.chen@oracle.com>
2015-06-08 11:24:38 -0700

    Bug 20147971 PSR:OSB fix - DatabindingImpl should cached MessageContextFactory/SAAJFactory for creating response packet.

--- jaxws-ri/eclipselink_jaxb/src/test/java/com/sun/xml/ws/api/databinding/DatabindingTest.java
+++ jaxws-ri/eclipselink_jaxb/src/test/java/com/sun/xml/ws/api/databinding/DatabindingTest.java
@@ -42,12 +42,17 @@ package com.sun.xml.ws.api.databinding;
 
 import javax.jws.WebService;
 import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
 
 import com.oracle.webservices.api.databinding.DatabindingFactory;
 import com.oracle.webservices.api.databinding.WSDLGenerator;
+import com.oracle.webservices.api.message.MessageContext;
+import com.oracle.webservices.api.message.MessageContextFactory;
 
 import com.sun.xml.ws.InVmWSDLResolver;
-
+import com.sun.xml.ws.api.message.Packet;
+import com.sun.xml.ws.message.saaj.SAAJMessage;
 
 import junit.framework.TestCase;
 
@@ -83,5 +88,33 @@ public class DatabindingTest extends TestCase {
         res.print();
         assertEquals(1, res.getAll().size());
         }
+    }
+    
+    static class MySaajFac extends com.sun.xml.ws.api.message.saaj.SAAJFactory {
+       boolean called = false;
+       public SAAJMessage readAsSAAJ(Packet packet) throws SOAPException {
+         called = true;
+         return super.readAsSAAJ(packet);
+       }
+    }    
+    public void testMessageContextFactory() throws Exception {
+      DatabindingFactory fac = DatabindingFactory.newInstance();
+      Databinding.Builder builder = fac.createBuilder(Hello2.class, null);
+      builder.targetNamespace("mytns");
+      builder.serviceName(new QName("mytns", "myservice"));
+      MessageContextFactory mcf = MessageContextFactory.createFactory();
+      MySaajFac saajFac = new MySaajFac();
+      mcf.setSAAJFactory(saajFac);
+      builder.property("com.sun.xml.ws.api.message.MessageContextFactory", mcf);
+      com.oracle.webservices.api.databinding.Databinding db = builder.build();
+      assertTrue(((Databinding)db).getMessageContextFactory() == mcf);
+      Class[] paramType = {String.class};
+      Object[] params = { "echoResponse" };
+      com.oracle.webservices.api.databinding.JavaCallInfo call = 
+          db.createJavaCallInfo(Hello2.class.getMethod("echo", paramType), params); 
+      call.setReturnValue("echoResponse");
+      MessageContext mc = db.serializeResponse(call);
+      SOAPMessage soap = mc.getAsSOAPMessage();
+      assertTrue(saajFac.called);
     }
 }
