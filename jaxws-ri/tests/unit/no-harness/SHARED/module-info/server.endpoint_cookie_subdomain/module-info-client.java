01d17662244825c1ff5cb0b34c445da9fca9f4f2
Miroslav Kos <miroslav.kos@oracle.com>
2015-09-10 18:32:51 +0200

    no-harness: adding required exports into test descriptors

--- jaxws-ri/tests/unit/no-harness/SHARED/module-info/server.endpoint_cookie_subdomain/module-info-client.java
+++ jaxws-ri/tests/unit/no-harness/SHARED/module-info/server.endpoint_cookie_subdomain/module-info-client.java
@@ -1,4 +1,6 @@
 module client {
     requires java.xml.ws;
     requires java.logging;
+
+    exports server.endpoint_cookie_subdomain.client;
 }
--- jaxws-ri/tests/unit/no-harness/SHARED/module-info/server.fault_to_endpoint/module-info-client.java
+++ jaxws-ri/tests/unit/no-harness/SHARED/module-info/server.fault_to_endpoint/module-info-client.java
@@ -1,4 +1,6 @@
 module client {
     requires java.xml.ws;
     requires java.logging;
+
+    exports server.endpoint1.client;
 }
