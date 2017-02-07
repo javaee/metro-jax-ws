175cf8966333c987dab25a1e403c44d6121c2820
Rajagopal Nallan Chakravarthy <rajagopal.nallan.chakravarthy@oracle.com>
2017-02-07 12:03:00 +0530

    fix_24845087

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/model/SOAPSEIModel.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/model/SOAPSEIModel.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -48,6 +48,8 @@ import javax.xml.namespace.QName;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Creates SOAP specific RuntimeModel
@@ -55,6 +57,8 @@ import java.util.Set;
  * @author Vivek Pandey
  */
 public class SOAPSEIModel extends AbstractSEIModelImpl {
+	
+	private final Lock lock = new ReentrantLock();
 
     public SOAPSEIModel(WebServiceFeatureList features) {
         super(features);
@@ -87,15 +91,22 @@ public class SOAPSEIModel extends AbstractSEIModelImpl {
 
     public Set<QName> getKnownHeaders() {
         Set<QName> headers = new HashSet<QName>();
-        for (JavaMethodImpl method : getJavaMethods()) {
-            // fill in request headers
-            Iterator<ParameterImpl> params = method.getRequestParameters().iterator();
-            fillHeaders(params, headers, Mode.IN);
+        
+        try{
+        	lock.lock();
+            for (JavaMethodImpl method : getJavaMethods()) {
+             // fill in request headers
+             Iterator<ParameterImpl> params = method.getRequestParameters().iterator();
+             fillHeaders(params, headers, Mode.IN);
 
             // fill in response headers
-            params = method.getResponseParameters().iterator();
-            fillHeaders(params, headers, Mode.OUT);
-        }
+             params = method.getResponseParameters().iterator();
+             fillHeaders(params, headers, Mode.OUT);
+			  }
+        }finally
+        {
+        	lock.unlock();
+         }
         return headers;
     }
 
