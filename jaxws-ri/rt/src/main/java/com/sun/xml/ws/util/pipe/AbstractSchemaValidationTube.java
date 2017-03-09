53f2f896c8839d2e825634af32ce556d973eb339
Eric Guo <shihua.guo@oracle.com>
2017-03-08 18:46:33 -0800

    Add null check for bug 25376108

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/util/pipe/AbstractSchemaValidationTube.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/util/pipe/AbstractSchemaValidationTube.java
@@ -498,7 +498,7 @@ public abstract class AbstractSchemaValidationTube extends AbstractFilterTubeImp
         assert docs.size() > 1;
         
         final StringBuilder sb = new StringBuilder("<xsd:schema xmlns:xsd='http://www.w3.org/2001/XMLSchema'");
-        if (!("".equals(tns)) && !("null".equals(tns))) {
+        if (tns != null && !("".equals(tns)) && !("null".equals(tns))) {
             sb.append(" targetNamespace='").append(tns).append("'");
         }
         sb.append(">\n");
