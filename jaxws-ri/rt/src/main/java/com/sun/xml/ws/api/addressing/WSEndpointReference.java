3408d04c31c4f1ecf61305214117d6ec4c86ad9f
Vaibhav Vishal <36727272+vavishal@users.noreply.github.com>
2018-02-26 13:34:37 +0530

    Fix for bug 27214864 (#1212)
    * Fix for bug 27214864

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/api/addressing/WSEndpointReference.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/api/addressing/WSEndpointReference.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2018 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -966,7 +966,7 @@ public final class WSEndpointReference  implements WSDLExtension {
      */
     public void addReferenceParametersToList(MessageHeaders outbound) {
         for (Header header : referenceParameters) {
-            outbound.add(header);
+            outbound.addOrReplace(header);
         }
     }
     /**
