e0b192fa3e25564d8e5e5491ab48cf4c6fe3d211
Roman Grigoriadi <roman.grigoriadi@oracle.com>
2018-07-10 13:29:25 +0200

    Modularization rt-fi, rt-ha and servlet.
    Signed-off-by: Roman Grigoriadi <roman.grigoriadi@oracle.com>

--- jaxws-ri/rt-fi/src/main/java/module-info.java
+++ jaxws-ri/rt-fi/src/main/java/module-info.java
@@ -39,32 +39,12 @@
  */
 
 /**
- * Defines tools for JAX-WS classes and WSDL generation,
- * including the <em>{@index wsgen wsgen tool}</em>
- * and <em>{@index wsimport wsimport tool}</em> tools.
- *
+ * Fast Infoset Support for JAX-WS RI
  *
  * @since 2.4.0
  */
-module com.sun.tools.ws {
+module com.sun.xml.ws.fi {
     requires com.sun.xml.ws;
-    requires javax.jws;
-    requires com.sun.istack.runtime;
-    requires com.sun.istack.tools;
-    requires com.sun.codemodel;
-    requires com.sun.tools.jxc;
-    requires com.sun.tools.xjc;
-    requires com.sun.xml.xsom;
-    requires com.sun.xml.ws.policy;
-    requires java.compiler;
-    requires java.logging;
-    requires java.rmi;
-    requires java.xml;
-    requires java.xml.bind;
-    requires java.xml.ws;
-
-    uses com.sun.tools.ws.wscompile.Plugin;
-
-    provides com.sun.tools.ws.wscompile.Plugin with
-            com.sun.tools.ws.wscompile.plugin.at_generated.PluginImpl;
+    requires com.sun.xml.streambuffer;
+    requires com.sun.xml.fastinfoset;
 }
